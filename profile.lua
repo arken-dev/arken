4ef443e93668c9fc60ca111c7cb954ab73587bb5
Alexandre Riveira <alexandre@objectdata.com.br>
2016-03-31 19:32:39 -0300

    profile

--- profile.lua
+++ profile.lua
@@ -3,4 +3,4 @@ package.cpath = package.cpath .. ";" .. OBERON_PATH .. "/clib/?.so"
 package.cpath = package.cpath .. ";" .. OBERON_PATH .. "/clib/?.dll"
 package.path  = package.path  .. ";" .. OBERON_PATH .. "/tasks/?.lua"
 
-require 'extensions'
+require 'oberon.base'
