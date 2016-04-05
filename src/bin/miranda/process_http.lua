388c95e720ee53384653b344d8e0d0264034c12e
Alexandre Riveira <alexandre@objectdata.com.br>
2016-04-05 08:21:22 -0300

    Query-String

--- src/bin/miranda/process_http.lua
+++ src/bin/miranda/process_http.lua
@@ -1,5 +1,5 @@
 process_http = function()
   return 200,
     {'Content-Type: text/html; charset=utf-8'},
-    request['QUERY_STRING'] .. '<br>' .. 'User-Agent:' .. request['User-Agent']
+    request['Query-String'] .. '<br>' .. 'User-Agent:' .. request['User-Agent']
 end
