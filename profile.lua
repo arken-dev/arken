ead6d37349914938645a705a77fa914373ea0560
Alexandre Riveira <alexandre@objectdata.com.br>
2016-04-05 17:58:06 -0300

    profile qt classes

--- profile.lua
+++ profile.lua
@@ -4,3 +4,28 @@ package.cpath = package.cpath .. ";" .. OBERON_PATH .. "/clib/?.dll"
 package.path  = package.path  .. ";" .. OBERON_PATH .. "/tasks/?.lua"
 
 require 'oberon.base'
+
+-------------------------------------------------------------------------------
+-- String
+-------------------------------------------------------------------------------
+
+require "QString"
+require "QByteArrayList"
+require "QStringList"
+require "QRegExp"
+
+-------------------------------------------------------------------------------
+-- QDir and QFile
+-------------------------------------------------------------------------------
+
+require "QDir"
+require "QDirIterator"
+require "QFile"
+require "QFileInfo"
+require "QFileInfoList"
+
+-------------------------------------------------------------------------------
+-- QDate, QTime and QDateTime
+-------------------------------------------------------------------------------
+
+require "QDateTime"
