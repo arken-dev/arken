d88c54919c6bd15293cf18e44d8f4e8bd2c03f61
Alexandre Riveira <alexandre@objectdata.com.br>
2016-04-08 08:24:03 -0300

    OThread

---
+++ include/oberon/othread.h
@@ -0,0 +1,14 @@
+#include <luajit-2.0/lua.hpp>
+#include <QtCore>
+#include <QThread>
+
+class OThread : public QThread
+{
+  public:
+  QByteArray   m_oberonPath;
+  QByteArray   m_fileName;
+  lua_State  * m_luaState;
+
+  explicit OThread(QByteArray oberonPath, QByteArray fileName);
+  void run();
+};
