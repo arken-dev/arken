5e53f1766d363cfe48ca47eef2dfea22c98ec27d
Alexandre Riveira <alexandre@objectdata.com.br>
2016-04-20 09:52:56 -0300

    helpers

---
+++ lib/template/helpers.lua
@@ -0,0 +1,7 @@
+-- http://stackoverflow.com/questions/4201142/rails-style-template-helpers-in-velocity
+-- http://framework.zend.com/manual/current/en/modules/zend.view.helpers.html
+-- http://framework.zend.com/manual/current/en/modules/zend.view.helpers.url.html
+
+local M = {}
+
+return M
