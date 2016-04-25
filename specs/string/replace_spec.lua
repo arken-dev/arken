ee98af606eadcc62e9e7df79323e9fc2b8fa8e32
Alexandre Riveira <alexandre@objectdata.com.br>
2016-04-25 19:41:20 -0300

    spec string#replace

---
+++ specs/string/replace_spec.lua
@@ -0,0 +1,16 @@
+local should = require "test.should"
+local test   = {}
+
+test['should replace space dot'] = function()
+  str1 = "this is a test"
+  str2 = "this.is.a.test"
+  should.be_equal( str1:replace(" ", "."), str2 )
+end
+
+test['should replace letter before and end'] = function()
+  str1 = "this is a test"
+  str2 = "-his is a -es-"
+  should.be_equal( str1:replace("t", "-"), str2 )
+end
+
+return test
