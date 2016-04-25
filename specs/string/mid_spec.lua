9ab923a967dfdd32359f9f29b2e279b522b17b89
Alexandre Riveira <alexandre@objectdata.com.br>
2016-04-25 19:06:31 -0300

    spec string#left

---
+++ specs/string/mid_spec.lua
@@ -0,0 +1,22 @@
+local should = require "test.should"
+local test   = {}
+
+test['should before of string'] = function()
+  str1 = "this is a test"
+  str2 = "this is"
+  should.be_equal( str1:mid(0, 7), str2 )
+end
+
+test['should middle of string'] = function()
+  str1 = "this is a test"
+  str2 = "is a"
+  should.be_equal( str1:mid(5, 4), str2 )
+end
+
+test['should end of string'] = function()
+  str1 = "this is a test"
+  str2 = "test"
+  should.be_equal( str1:mid(10, 4), str2 )
+end
+
+return test
