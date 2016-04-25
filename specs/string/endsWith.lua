693af295741423420bc32b2329c25f1fe06015ce
Alexandre Riveira <alexandre@objectdata.com.br>
2016-04-25 18:34:58 -0300

    spec string#endsWith

---
+++ specs/string/endsWith.lua
@@ -0,0 +1,24 @@
+local should = require "test.should"
+local test   = {}
+
+test['should true for a extension image'] = function()
+  str1 = "photo.png"
+  should.be_valid( str1:endsWith(".png") )
+end
+
+test['should false for a invalid extension image'] = function()
+  str1 = "photo.png"
+  should.not_valid( str1:endsWith(".gif") )
+end
+
+test['should true for a extension image with new line'] = function()
+  str1 = "photo.png\n"
+  should.be_valid( str1:endsWith(".png\n") )
+end
+
+test['should true for a extension image with return and new line'] = function()
+  str1 = "photo.png\r\n"
+  should.be_valid( str1:endsWith(".png\r\n") )
+end
+
+return test
