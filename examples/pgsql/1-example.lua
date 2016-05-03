96586ec4e9b4edb8dbe2257ad15f3c395018459e
Alexandre Riveira <alexandre@objectdata.com.br>
2016-05-03 07:00:18 -0300

    example

---
+++ examples/pgsql/1-example.lua
@@ -0,0 +1,8 @@
+require 'oberon.pgsql'
+
+connect = pg.connect('dbname=fusion_development user=postgres password=42 host=localhost')
+query   = [[SELECT attname FROM pg_attribute]]
+result  = connect:exec(query)
+for row in result:rows() do
+  print(row.attname)
+end 
