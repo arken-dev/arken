09e5f97c3a3e02933989502f213794229dfb256f
Alexandre Riveira <alexandre@objectdata.com.br>
2016-05-14 16:04:23 -0300

    camelcase specs review

--- specs/string/camelcase.lua
+++ specs/string/camelcase.lua
@@ -2,27 +2,50 @@ local should = require "test.should"
 local test   = {}
 
 test['should upper case in underline'] = function()
-  str1 = "this_is_a_test"
-  str2 = "ThisIsATest"
+  local str1 = "this_is_a_test"
+  local str2 = "ThisIsATest"
   should.be_equal( str1:camelcase(), str2 )
 end
 
 test['should upper case in bar'] = function()
-  str1 = "pedido/corporativo"
-  str2 = "Pedido.Corporativo"
+  local str1 = "active_record"
+  local str2 = "ActiveRecord"
   should.be_equal( str1:camelcase(), str2 )
 end
 
 test['should upper case in underline and bar'] = function()
-  str1 = "pedido_regra/item"
-  str2 = "PedidoRegra.Item"
+  local str1 = "active_record/adapter"
+  local str2 = "ActiveRecord.Adapter"
   should.be_equal( str1:camelcase(), str2 )
 end
 
-test['should upper first case'] = function()
-  str1 = "classes/OByteArray"
-  str2 = "Classes.OByteArray"
+test['should keep camelcase chars'] = function()
+  local str1 = "classes/OByteArray"
+  local str2 = "Classes.OByteArray"
   should.be_equal( str1:camelcase(), str2 )
+end
+
+test['should return empty string'] = function()
+  local str1 = ""
+  local str2 = ""
+  should.be_equal( str1:camelcase(), str2 )
+end
+
+test['should return one string'] = function()
+  local str1 = "c"
+  local str2 = "C"
+  should.be_equal( str1:camelcase(), str2 )
+end
+
+local examples = {
+  html_api = 'HtmlApi',
+  HTML_api = 'HTMLApi',
+}
+
+for source, destination in pairs(examples) do
+  test['should convert ' .. source .. ' to ' .. destination] = function()
+    should.be_equal(source:camelcase(), destination)
+  end
 end
 
 return test
