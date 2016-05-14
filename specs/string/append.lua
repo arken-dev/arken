81d4fcdaf6ed4c35e4f48d34e13425d9a2b70bf9
Alexandre Riveira <alexandre@objectdata.com.br>
2016-05-14 16:10:40 -0300

    string append tests

--- specs/string/append.lua
+++ specs/string/append.lua
@@ -2,17 +2,26 @@ local should = require "test.should"
 local test   = {}
 
 test['should append new string'] = function()
-  str1 = "this is a test"
-  str2 = "this is a test"
+  local str1 = "this is a test"
+  local str2 = "this is a test"
   should.be_equal( str1:append(str2), "this is a testthis is a test" )
 end
 
 test['should tree strings'] = function()
-  str1 = "this is a test"
-  str2 = " "
-  str3 = "this is a test"
+  local str1 = "this is a test"
+  local str2 = " "
+  local str3 = "this is a test"
   should.be_equal( str1:append(str2):append(str3), "this is a test this is a test" )
 end
 
+test['should append empty string'] = function()
+  local str1 = "this is a test"
+  local str2 = ""
+  should.be_equal( str1:append(str2), "this is a test" )
+
+  local str1 = ""
+  local str2 = "this is a test"
+  should.be_equal( str1:append(str2), "this is a test" )
+end
 
 return test
