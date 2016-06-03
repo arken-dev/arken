0a07059781d75eee11df495ab845d15bfacc8b44
Alexandre Riveira <alexandre@objectdata.com.br>
2016-06-03 10:24:30 -0300

    OBERON_ENV profile

--- profile.lua
+++ profile.lua
@@ -1,7 +1,17 @@
+-------------------------------------------------------------------------------
+-- PATH
+-------------------------------------------------------------------------------
+
 package.path  = package.path  .. ";" .. OBERON_PATH .. "/lib/?.lua"
+package.path  = package.path  .. ";" .. OBERON_PATH .. "/tasks/?.lua"
 package.cpath = package.cpath .. ";" .. OBERON_PATH .. "/clib/?.so"
 package.cpath = package.cpath .. ";" .. OBERON_PATH .. "/clib/?.dll"
-package.path  = package.path  .. ";" .. OBERON_PATH .. "/tasks/?.lua"
+
+-------------------------------------------------------------------------------
+-- ENV
+-------------------------------------------------------------------------------
+
+OBERON_ENV = os.getenv("OBERON_ENV") or "development"
 
 -------------------------------------------------------------------------------
 -- Base
