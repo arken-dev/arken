97d6611538a13e7df3110092c9a94e66d06b014e
Alexandre Riveira <alexandre@objectdata.com.br>
2016-06-04 10:11:24 -0300

    math refactory

--- src/oberon/base/math.cpp
+++ src/oberon/base/math.cpp
@@ -1,55 +1,49 @@
 #include <cmath>
 #include <oberon/helper>
 
-int    (*cabs)   (int)    = &abs;
-double (*cacos)  (double) = &acos;
-double (*casin)  (double) = &asin;
-double (*catan)  (double) = &atan;
-double (*catan2) (double, double) = &atan2;
-double (*cceil)  (double) = &ceil;
-double (*cfloor) (double) = &floor;
-
 int math::abs(int number)
 {
-  return cabs(number);
+  return std::abs(number);
 }
 
 double math::acos(double number)
 {
-  return cacos(number);
+  return std::acos(number);
 }
 
 double math::asin(double number)
 {
-  return casin(number);
+  return std::asin(number);
 }
 
 double math::atan(double number)
 {
-  return catan(number);
+  return std::atan(number);
 }
 
 double math::atan2(double x, double y)
 {
-  return catan2(x, y);
+  return std::atan2(x, y);
 }
 
 double math::ceil(double number)
 {
-  return cceil(number);
+  return std::ceil(number);
 }
 
 double math::floor(double number)
 {
-  return cfloor(number);
+  return std::floor(number);
 }
 
 double math::round(double number, int precision)
 {
-  return cfloor((number + (0.5/(pow(10, precision)))) * (pow(10, precision))) / (pow(10, precision));
+  double dprecision = std::pow(10, precision);
+  return std::floor((number + (0.5/(dprecision))) * (dprecision)) / dprecision;
 }
 
 double math::truncate(double number, int precision)
 {
-  return cfloor(number * (pow(10, precision))) / (pow(10, precision));
+  double dprecision = std::pow(10, precision);
+  return std::floor(number * (dprecision)) / dprecision;
 }
