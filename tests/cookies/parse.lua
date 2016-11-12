4b334a79489bc4c0af735505d05234716a6b9351
Alexandre Riveira <alexandre@objectdata.com.br>
2016-11-12 19:09:31 -0200

    fix tests

--- tests/cookies/parse.lua
+++ tests/cookies/parse.lua
@@ -1,14 +1,14 @@
-local cookie =require 'oberon.cookie'
+local cookie = require 'oberon.cookie'
 local test   = {}
 
 test['should return __gads'] = function()
-  local buffer = os.read(OBERON_PATH .. "/specs/cookies/1-example.cookie")
+  local buffer = os.read(OBERON_PATH .. "/tests/cookies/1-example.cookie")
   local result = cookie.parse(buffer)
   assert(result.__gads == "ID=65db397af496b42f:T=1459784587:S=ALNI_MaXlE55QEou3h9txkA8jA5kPzMKVg")
 end
 
 test['return _session_id'] = function()
-  local buffer = os.read(OBERON_PATH .. "/specs/cookies/1-example.cookie")
+  local buffer = os.read(OBERON_PATH .. "/tests/cookies/1-example.cookie")
   local result = cookie.parse(buffer)
   assert(result._session_id == "5c08fc52513b2d562c62a1f7252dc834")
 end
