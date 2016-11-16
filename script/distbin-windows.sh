3b25cc7b194db7b28167a15864442f738b65326f
Alexandre Riveira <alexandre@objectdata.com.br>
2016-11-16 13:08:59 -0200

    distbin

--- script/distbin-windows.sh
+++ script/distbin-windows.sh
@@ -1,2 +1,10 @@
-cp vendors/lua51.dll bin
-cp vendors/oberon.dll bin
+cp vendors/*.dll bin
+cp $1/bin/libgcc_s_dw2-1.dll bin
+cp $1/bin/libstdc++-6.dll bin
+cp $1/bin/libwinpthread-1.dll bin
+cp $1/bin/Qt5Concurrent.dll bin
+cp $1/bin/Qt5Concurrentd.dll bin
+cp $1/bin/Qt5Core.dll bin
+cp $1/bin/Qt5Cored.dll bin
+cp $1/bin/Qt5Network.dll bin
+cp $1/bin/Qt5Networkd.dll bin
