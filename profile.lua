0807ae50201c91615df8a182195f8b642245d2b9
Alexandre Riveira <alexandre@objectdata.com.br>
2016-11-16 19:57:41 -0200

    remove path profile

--- profile.lua
+++ profile.lua
@@ -6,7 +6,6 @@ package.path  = package.path  .. ";" .. OBERON_PATH .. "/lib/?.lua"
 package.path  = package.path  .. ";" .. OBERON_PATH .. "/rhea/?.lua"
 package.cpath = package.cpath .. ";" .. OBERON_PATH .. "/clib/?.so"
 package.cpath = package.cpath .. ";" .. OBERON_PATH .. "/clib/?.dll"
-package.path  = package.path  .. ";rhea/?.lua"
 
 -------------------------------------------------------------------------------
 -- ENV
