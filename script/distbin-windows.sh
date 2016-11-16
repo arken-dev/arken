618040d69a906b720ddd905d303adf42bebe7bf2
Alexandre Riveira <alexandre@objectdata.com.br>
2016-11-16 19:23:23 -0200

    improve oberon-dist

--- script/distbin-windows.sh
+++ script/distbin-windows.sh
@@ -1,15 +1,17 @@
-cp vendors/*.dll bin
-cp $1/bin/libgcc_s_dw2-1.dll bin
-cp $1/bin/libstdc++-6.dll bin
-cp $1/bin/libwinpthread-1.dll bin
-cp $1/bin/Qt5Concurrent.dll bin
-cp $1/bin/Qt5Concurrentd.dll bin
-cp $1/bin/Qt5Core.dll bin
-cp $1/bin/Qt5Cored.dll bin
-cp $1/bin/Qt5Network.dll bin
-cp $1/bin/Qt5Networkd.dll bin
-cp $1/bin/Qt5Widgets.dll bin
-cp $1/bin/Qt5Widgetsd.dll bin
-cp $1/bin/Qt5Gui.dll bin
-cp $1/bin/Qt5Guid.dll bin
-cp -r $1/plugins/platforms bin
+cd ..
+cp -pr oberon oberon-dist
+cp oberon/vendors/*.dll oberon-dist/bin
+cp $1/bin/libgcc_s_dw2-1.dll oberon-dist/bin
+cp $1/bin/libstdc++-6.dll oberon-dist/bin
+cp $1/bin/libwinpthread-1.dll oberon-dist/bin
+cp $1/bin/Qt5Concurrent.dll oberon-dist/bin
+cp $1/bin/Qt5Concurrentd.dll oberon-dist/bin
+cp $1/bin/Qt5Core.dll oberon-dist/bin
+cp $1/bin/Qt5Cored.dll oberon-dist/bin
+cp $1/bin/Qt5Network.dll oberon-dist/bin
+cp $1/bin/Qt5Networkd.dll oberon-dist/bin
+cp $1/bin/Qt5Widgets.dll oberon-dist/bin
+cp $1/bin/Qt5Widgetsd.dll oberon-dist/bin
+cp $1/bin/Qt5Gui.dll oberon-dist/bin
+cp $1/bin/Qt5Guid.dll oberon-dist/bin
+cp -r $1/plugins/platforms oberon-dist/bin
