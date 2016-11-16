0b1ceb59e6658109cf2950880d12902fb27a864d
Alexandre Riveira <alexandre@objectdata.com.br>
2016-11-16 13:30:44 -0200

    script

--- script/distbin-windows.sh
+++ script/distbin-windows.sh
@@ -8,3 +8,8 @@ cp $1/bin/Qt5Core.dll bin
 cp $1/bin/Qt5Cored.dll bin
 cp $1/bin/Qt5Network.dll bin
 cp $1/bin/Qt5Networkd.dll bin
+cp $1/bin/Qt5Widgets.dll bin
+cp $1/bin/Qt5Widgetsd.dll bin
+cp $1/bin/Qt5Gui.dll bin
+cp $1/bin/Qt5Guid.dll bin
+cp -r $1/plugins/platforms bin
