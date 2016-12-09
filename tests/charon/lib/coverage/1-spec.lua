d3bf0330d3fba9de4ec46d6237612dfc076ee8e4
Alexandre Riveira <alexandre@objectdata.com.br>
2016-12-09 21:06:58 -0200

    tests

--- tests/charon/lib/coverage/1-spec.lua
+++ tests/charon/lib/coverage/1-spec.lua
@@ -1,31 +1,34 @@
 local coverage = require 'charon.coverage'
-local should   = require 'test.should'
-local test   = {}
+local test = {}
 
 coverage.start()
-loadfile('specs.coverage.1-example.example')
+require('tests.charon.lib.coverage.1-example')
 coverage.stop()
 
 test['should return the table type of name file'] = function()
   local result = coverage.dump()
-  assert(type(result['@./specs/coverage/1-example.lua']) == 'table')
+  local buffer = ""
+  for k, v in pairs(result) do
+    buffer = buffer .. k
+  end
+  assert(type(result['@./tests/charon/lib/coverage/1-example.lua']) == 'table')
 end
 
 test['should return flag 1 for line 1'] = function()
-  local analyze = coverage.analyze('./specs/coverage/1-example.lua')
-  assert(analyze[1].line == 'flag = true')
-  assert(analyze[2].flag == 1)
+  local analyze = coverage.analyze('@./tests/charon/lib/coverage/1-example.lua').lines
+  assert(analyze[1].src == 'flag = true', json.encode(analyze[1]))
+  assert(analyze[1].flag == 1, json.encode(analyze[1]))
 end
 
 test['should return flag -1 for line 5'] = function()
-  local analyze = coverage.analyze('./specs/coverage/1-example.lua')
-  assert(analyze[5].line == 'else')
-  assert(analyze[5].flag == -1)
+  local analyze = coverage.analyze('@./tests/charon/lib/coverage/1-example.lua').lines
+  assert(analyze[5].src == 'else', analyze[5])
+  assert(analyze[5].flag == -1, analyze[5])
 end
 
 test['should return flag nil for line 6'] = function()
-  local analyze = coverage.analyze('./specs/coverage/1-example.lua')
-  assert(analyze[6].line == "  var = 'false..'")
+  local analyze = coverage.analyze('@./tests/charon/lib/coverage/1-example.lua').lines
+  assert(analyze[6].src == "  var = 'false..'")
   assert(analyze[6].flag == nil)
 end
 
