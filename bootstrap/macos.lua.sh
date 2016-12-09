87d58b6745cab10e69f7e05e318b51eb685a02a9
Ana Paula <anapaulasanchesriveira@MacBook-Pro-de-Ana-Paula-Sanches-Riveira.local>
2016-12-09 17:11:42 -0200

    bootstrap

--- bootstrap/macos.lua.sh
+++ bootstrap/macos.lua.sh
@@ -3,10 +3,13 @@ curl http://www.lua.org/ftp/lua-5.1.5.tar.gz -o tmp.tar.gz
 tar -xzvf tmp.tar.gz
 cd lua-5.1.5
 make macosx test
+make install INSTALL_TOP=$PWD/../lua
+mkdir -p ../../deps/include
+cp -pr ../lua/include ../../deps/include/lua
 cd src
 mkdir tmp
 cp liblua.a tmp
 cd tmp
 ar -x liblua.a
 gcc -shared *.o -o liblua.so
-cp liblua.so ../../../../vendors/liblua.so
+cp liblua.so ../../../../deps/liblua.so
