5eb74e33072da6033f77cdd32e6b5d73a23e2432
Alexandre Riveira <alexandre@objectdata.com.br>
2016-12-14 20:00:31 -0200

    remove disabled code

--- profile.lua
+++ profile.lua
@@ -24,8 +24,6 @@ require 'charon.helper'
 require 'charon.package'
 require 'charon.parse'
 require "CStringList"
---require "Class"
---require "Object"
 
 -------------------------------------------------------------------------------
 -- PROFILE.D
