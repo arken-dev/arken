072020816f198a7b373dfd574ae6d8a0ab575011
Alexandre Riveira <alexandre@objectdata.com.br>
2016-12-17 13:25:47 -0200

    os

--- include/charon/base/os.h
+++ include/charon/base/os.h
@@ -3,39 +3,40 @@
 
 namespace charon {
   namespace base {
-    namespace os {
-      char   * abspath(const char * path);
-      uint     atime(const char * path);
-      bool     compare(const char * path1, const char * path2);
-      bool     copy(const char * source, const char * destination, bool force);
-      int      cores();
-      bool     chdir(const char * dirpath);
-      uint     ctime(const char * path);
-      char   * dirpath(const char * path);
-      bool     exists(const char * path);
-      CStringList * glob(const char * dir);
-      CStringList * glob(const char * dir, bool sub);
-      CStringList * glob(const char * dir, const char * regex);
-      CStringList * glob(const char * dir, const char * regex, bool sub);
-      char   * home();
-      char   * hostname();
-      bool     link(const char * source, const char * destination, bool force);
-      double   microtime();
-      bool     mkdir(const char * dirname);
-      bool     mkpath(const char * dirpath);
-      char   * name();
-      qint64   pid();
-      char   * pwd();
-      bool     rmdir(const char * dirname);
-      bool     rmpath(const char * dirpath);
-      void     sleep(double secs);
-      char   * target(const char * path);
-      char   * temp();
-      bool     touch(const char * path);
-      char   * uuid();
-      char   * read(const char * path);
-      char   * read(const char * path, size_t * size);
-      char   * root();
+    class os {
+      public:
+      static char   * abspath(const char * path);
+      static uint     atime(const char * path);
+      static bool     compare(const char * path1, const char * path2);
+      static bool     copy(const char * source, const char * destination, bool force);
+      static int      cores();
+      static bool     chdir(const char * dirpath);
+      static uint     ctime(const char * path);
+      static char   * dirpath(const char * path);
+      static bool     exists(const char * path);
+      static CStringList * glob(const char * dir);
+      static CStringList * glob(const char * dir, bool sub);
+      static CStringList * glob(const char * dir, const char * regex);
+      static CStringList * glob(const char * dir, const char * regex, bool sub);
+      static char   * home();
+      static char   * hostname();
+      static bool     link(const char * source, const char * destination, bool force);
+      static double   microtime();
+      static bool     mkdir(const char * dirname);
+      static bool     mkpath(const char * dirpath);
+      static char   * name();
+      static qint64   pid();
+      static char   * pwd();
+      static bool     rmdir(const char * dirname);
+      static bool     rmpath(const char * dirpath);
+      static void     sleep(double secs);
+      static char   * target(const char * path);
+      static char   * temp();
+      static bool     touch(const char * path);
+      static char   * uuid();
+      static char   * read(const char * path);
+      static char   * read(const char * path, size_t * size);
+      static char   * root();
     }
   }
 }
