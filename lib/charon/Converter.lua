4e53498165e91bc05945ceb9b432dedbd7668ad7
Alexandre Riveira <alexandre@objectdata.com.br>
2016-12-16 23:51:40 -0200

    copyright

--- lib/charon/Converter.lua
+++ lib/charon/Converter.lua
@@ -1,3 +1,8 @@
+-- Copyright 2016 The Charon Platform Authors.
+-- All rights reserved.
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
 local Class = require('charon.oop.Class')
 local json  = require('json')
 local Converter = Class.new("Converter")
