839d13cc9abc1643f55a7f73121910bdac0c54a6
Alexandre Riveira <alexandre@objectdata.com.br>
2016-12-23 18:04:53 -0200

    triton.lib.files

---
+++ lib/triton/coverage/lib/files.lua
@@ -0,0 +1,104 @@
+CHARON_ENV = os.getenv("CHARON_ENV") or "test"
+
+local QDirIterator = require 'QDirIterator'
+local QFileInfo    = require 'QFileInfo'
+local test         = require 'charon.test'
+local template     = require 'charon.template'
+local coverage     = require 'charon.coverage'
+local json         = require 'charon.jsonp'
+local start        = os.microtime()
+local files        = {}
+
+-------------------------------------------------------------------------------
+-- TRITON START
+-------------------------------------------------------------------------------
+
+function triton_start()
+  local dir = 'coverage'
+  if not os.exists(dir) then
+    os.mkdir(dir)
+  end
+  for i = 1, #arg do
+    local fileName = arg[i]
+    if fileName:suffix() == 'lua' then
+      print(fileName)
+      table.insert(files, fileName)
+      triton.enqueue(fileName)
+    end
+  end
+end
+
+function triton_run(fileName)
+  local tests     = {}
+  local dirName   = 'tests/' .. fileName:gsub(".lua", "")
+  if not os.exists(dirName) then
+    triton.append("message", string.format("warning: directory %s not exists\n", dirName))
+  end
+  local libName  = dirName:replace("tests/lib/", ""):gsub("/", "."):replace('.lua', '')
+  local iterator = QDirIterator.new(dirName)
+  print(libName)
+  package.loaded[libName] = nil
+
+  while(iterator:hasNext()) do
+    iterator:next()
+    local fileInfo = iterator:fileInfo()
+    if fileInfo:filePath():endsWith(".lua") then
+      table.insert(tests, fileInfo:filePath())
+    end
+  end
+
+  coverage.reset()
+  coverage.start()
+  local results = test.execute(tests)
+  coverage.stop()
+
+  for fileName, result in pairs(results) do
+    for description, result in pairs(result) do
+      triton.count('tests')
+      if result.status ~= 'ok' then
+        local buffer = description .. '\n'
+        if result.msg and tostring(result.msg):len() > 0  then
+          buffer = buffer .. tostring(result.msg) .. '\n'
+        end
+        triton.append('message', fileName .. '\n' .. buffer)
+      end
+
+      triton.count(result.status)
+    end
+  end
+
+  local dir     = 'coverage'
+  local file    = fileName
+  local tpl     = CHARON_PATH .. "/lib/charon/coverage/templates/file.html"
+  local dump    = coverage.dump()
+  local data    = coverage.analyze(fileName)
+  local buffer  = template.execute(tpl, data)
+  local file    = io.open((dir .. "/" .. data.file_name:replace("/", "-") .. '.html'), "w")
+  file:write(buffer)
+  file:close()
+
+  file = io.open((dir .. "/" .. data.file_name:replace("/", "-") .. '.json'), "w")
+  file:write(json.pretty(data))
+  file:close()
+end
+
+-------------------------------------------------------------------------------
+-- TRITON STOP
+-------------------------------------------------------------------------------
+
+function triton_stop()
+  print('')
+  local dir    = 'coverage'
+  local tpl    = CHARON_PATH .. "/lib/charon/coverage/templates/index.html"
+  local data   = {files = files, time = (os.microtime() - start), total = triton.total('tests')}
+  local buffer = template.execute(tpl, data)
+
+  local file   = io.open((dir .. "/" .. 'index.html'), "w")
+  file:write(buffer)
+  file:close()
+
+  local result = "%i tests, %i pendings, %i failures"
+  print('\n' .. triton.result('message'))
+  print(string.format(result, triton.total('test'), triton.total('failure'), triton.total('pending')))
+  print(string.format("Finished in %.2f seconds", os.microtime() - start))
+end
