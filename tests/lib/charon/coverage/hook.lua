b59df265020622076fbf3353f54981fcb0141a04
Alexandre Riveira <alexandre@objectdata.com.br>
2016-12-26 09:31:57 -0200

    coverage tool

--- tests/lib/charon/coverage/hook.lua
+++ tests/lib/charon/coverage/hook.lua
@@ -2,14 +2,15 @@ local test = {}
 local coverage = require('charon.coverage')
 local mydebug  = {}
 mydebug.info = function()
-  return "@lib/charon/coverage.lua", 2
+  return "@lib/charon/dontexistsfile.lua", 2
 end
 
 test.should_apply_linedefined = function()
+  coverage.reset()
   coverage.hook('', '', mydebug)
   coverage.hook('', '', mydebug)
   local result = coverage.dump()
-  assert( result['@lib/charon/coverage.lua'][2] == 1, result['@lib/charon/coverage.lua'][2] )
+  assert( result['@lib/charon/dontexistsfile.lua'][2] == 2, result['@lib/charon/dontexistsfile.lua'][2] )
 end
 
 return test
