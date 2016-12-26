b9cd7156ec153c5ea1313e8b7a7c91a2e5778ac4
Alexandre Riveira <alexandre@objectdata.com.br>
2016-12-26 11:21:40 -0200

    dir util

--- tests/lib/callisto/callisto.lua
+++ tests/lib/callisto/callisto.lua
@@ -4,16 +4,16 @@ local callisto = require 'callisto'
 test.should_error_not_exists_file = function()
   local test = require('charon.test')
   test.output = function() end
-  local result, message = pcall(callisto, "lib/callisto/example-not-exists.lua")
+  local result, message = pcall(callisto, "util/callisto/example-not-exists.lua")
   test.output = io.write
   assert(result == false)
-  assert(message:contains("lib/callisto/example-not-exists.lua not exists"), message)
+  assert(message:contains("util/callisto/example-not-exists.lua not exists"), message)
 end
 
 test.should_result_ok = function()
   local test = require('charon.test')
   test.output = function() end
-  local icon, result = callisto("lib/callisto/example-ok.lua")
+  local icon, result = callisto("util/callisto/example-ok.lua")
   test.output = io.write
   assert(icon == "ok", icon)
 end
@@ -21,7 +21,7 @@ end
 test.should_result_warning = function()
   local test = require('charon.test')
   test.output = function() end
-  local icon, result = callisto("lib/callisto/example-warning.lua")
+  local icon, result = callisto("util/callisto/example-warning.lua")
   test.output = io.write
   assert(icon == "warning", icon)
 end
@@ -29,10 +29,10 @@ end
 test.should_result_failure = function()
   local test = require('charon.test')
   test.output = function() end
-  local icon, result = callisto("lib/callisto/example-failure.lua")
+  local icon, result = callisto("util/callisto/example-failure.lua")
   test.output = io.write
   assert(icon == "failure", icon)
-  assert(result:contains("lib/callisto/example-failure.lua:4: failure"), result)
+  assert(result:contains("util/callisto/example-failure.lua:4: failure"), result)
 end
 
 return test
