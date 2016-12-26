ecc162e11e023fd3ad19d3188190398118a53740
Alexandre Riveira <alexandre@objectdata.com.br>
2016-12-26 11:38:42 -0200

    coverage isblank

---
+++ tests/lib/charon/isblank/tests.lua
@@ -0,0 +1,32 @@
+local isblank = require('charon.isblank')
+local test = {}
+
+test.should_return_true_for_nil = function()
+  assert( isblank(nil) == true )
+end
+
+test.should_return_true_for_empty_string = function()
+  assert( isblank('') == true )
+end
+
+test.should_return_true_for_empty_table = function()
+  assert( isblank({}) == true )
+end
+
+test.should_return_true_for_boolean_false = function()
+  assert( isblank(false) == true )
+end
+
+test.should_return_false_for_string_with_space = function()
+  assert( isblank(' ') == false )
+end
+
+test.should_return_false_for_table_with_values = function()
+  assert( isblank({ id = 1 }) == false )
+end
+
+test.should_return_false_for_numbers = function()
+  assert( isblank(1) == false )
+end
+
+return test
