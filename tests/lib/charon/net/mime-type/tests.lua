fc84576af5f3652f984ecf0a25d79416e0497497
Alexandre Riveira <alexandre@objectdata.com.br>
2016-12-26 11:44:11 -0200

    coverage mime-type

---
+++ tests/lib/charon/net/mime-type/tests.lua
@@ -0,0 +1,12 @@
+local mimetype = require('charon.net.mime-type')
+local test = {}
+
+test.should_contains_text_mime_type = function()
+  assert( mimetype.text == 'text/plain', mimetype.text )
+end
+
+test.should_contains_html_mime_type = function()
+  assert( mimetype.html == 'text/html', mimetype.html )
+end
+
+return test
