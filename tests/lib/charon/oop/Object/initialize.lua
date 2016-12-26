bc309e7d45b19cecbae586eb04cdea543d634b77
Alexandre Riveira <alexandre@objectdata.com.br>
2016-12-26 19:27:56 -0200

    Object

---
+++ tests/lib/charon/oop/Object/initialize.lua
@@ -0,0 +1,18 @@
+local Class = require('charon.oop.Class')
+local test  = {}
+
+test.should_execute_initialize_instances = function()
+  local MyClass = Class.new("MyClass")
+  function MyClass:initialize()
+    self.instance_value = 'hey here !!!'
+  end
+  local obj = MyClass.new()
+  assert( obj.instance_value == 'hey here !!!' )
+  assert( obj.class == MyClass )
+end
+
+test.should_execute_object_return_nil = function()
+  assert( Object:initialize() == nil )
+end
+
+return test
