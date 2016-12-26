c9652f5d1a09ddb8bb17563a2da3f62485532e66
Alexandre Riveira <alexandre@objectdata.com.br>
2016-12-26 17:39:50 -0200

    colorize coverage

--- lib/charon/colorize.lua
+++ lib/charon/colorize.lua
@@ -14,10 +14,11 @@ colorize.blue      = '0;34'
 colorize.magenta   = '0;35'
 colorize.cyan      = '0;36'
 colorize.white     = '0;37'
+colorize.osName    = os.name()
 
 colorize.format = function(text, color)
   color = colorize[color]
-  if os.name() == 'windows' then
+  if colorize.osName == 'windows' then
     return text
   else
     return string.char(27)..'[' .. color .. 'm' .. text .. string.char(27) ..'[0m'
---
+++ tests/lib/charon/colorize/format.lua
@@ -0,0 +1,15 @@
+local colorize = require('charon.colorize')
+local test = {}
+
+test.should_return_text_without_color_if_windows = function()
+  colorize.osName = 'windows'
+  assert( colorize.format('my text', 'green') == 'my text', colorize.format('my text', 'green') )
+  colorize.osName = os.name()
+end
+
+test.should_return_text_without_color_if_not_windows = function()
+  local result = string.char(27)..'[' .. '0;32' .. 'm' .. 'my text' .. string.char(27) ..'[0m'
+  assert( colorize.format('my text', 'green') == result, colorize.format('my text', 'green') )
+end
+
+return test
