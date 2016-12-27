a8091b9e239dc0b1f78ab942b18e77e8d32c8004
Alexandre Riveira <alexandre@objectdata.com.br>
2016-12-27 10:26:16 -0200

    rhea task

---
+++ lib/rhea/Test.lua
@@ -0,0 +1,56 @@
+local Class = require 'charon.oop.Class'
+local Test  = Class.new("Test")
+
+-------------------------------------------------------------------------------
+-- HELP
+-------------------------------------------------------------------------------
+Test.help = {}
+
+-------------------------------------------------------------------------------
+-- SPLIT
+-------------------------------------------------------------------------------
+
+Test.help.split = [[
+  receive file name para and extrac all functions methods e create equivalente
+  file tests
+]]
+
+function Test:split()
+  local fileName = arg[1]
+  local module   = dofile(arg[1])
+  local filePath = 'tests/' .. fileName:mid(0, fileName:lastIndexOf("."))
+  print(fileName)
+  print(filePath)
+  os.mkpath(filePath)
+
+  if type(module) == 'table' then
+    for k, v in pairs(module) do
+      if type(v) == 'function' then
+        local fileTest = filePath .. '/' .. k .. '.lua'
+        if os.exists(fileTest) then
+          print( fileTest .. ' exists' )
+        else 
+          local file = io.open(fileTest, 'w')
+          file:write("local test = {}\nreturn test")
+          file:close()
+          print( fileTest .. ' create' )
+        end
+      end
+    end
+  end
+
+  if type(module) == 'function' then
+    local fileTest = filePath .. '/tests.lua'
+    if os.exists(fileTest) then
+      print( fileTest .. ' exists' )
+    else 
+      local file = io.open(fileTest, 'w')
+      file:write("local test = {}\nreturn test")
+      file:close()
+      print( fileTest .. ' create' )
+    end
+  end
+end
+
+
+return Test
