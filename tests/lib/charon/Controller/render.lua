db3419a7444def76b151d00d5e6b21a15becce25
Alexandre Riveira <alexandre@objectdata.com.br>
2016-12-29 12:50:00 -0200

    tests

--- tests/lib/charon/Controller/render.lua
+++ tests/lib/charon/Controller/render.lua
@@ -1,2 +1,14 @@
+local Controller = require('charon.Controller')
 local test = {}
+
+test.should_execute_output_method_prefix_render = function()
+  Controller.render_mycustomrender = function()
+    return "works !!!"
+  end
+  local c = Controller.new()
+  local result = c:render{ output = 'mycustomrender' }
+  Controller.render_mycustomrender = nil
+  assert( result == "works !!!" )
+end
+
 return test
