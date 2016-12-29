b5b49c535c47f570b5c4bd94dafb722cda5fcb5a
Alexandre Riveira <alexandre@objectdata.com.br>
2016-12-29 16:33:30 -0200

    tests

--- lib/charon/Controller.lua
+++ lib/charon/Controller.lua
@@ -3,11 +3,12 @@
 -- Use of this source code is governed by a BSD-style
 -- license that can be found in the LICENSE file.
 
-local Class    = require "charon.oop.Class"
-local template = require "charon.template"
-local url      = require "charon.net.url"
+local Class      = require "charon.oop.Class"
+local template   = require "charon.template"
+local url        = require "charon.net.url"
+local json       = require "charon.json"
+local Controller = Class.new("Controller")
 
-Controller = Class.new("Controller")
 Controller.prefixHelpers = "app.helpers."
 
 local helpers = {}
--- tests/lib/charon/Controller/render_json.lua
+++ tests/lib/charon/Controller/render_json.lua
@@ -1,2 +1,36 @@
+local Controller = require 'charon.Controller'
 local test = {}
+
+test.should_return_http_status_500_if_errors = function()
+  request = { params = function() return { json_callback = nil} end }
+  local c = Controller.new()
+  local object = { id = 1, name = 'John' }
+  local status, headers, body = c:render({ output = 'json', value = object })
+  assert( status == 200 )
+end
+
+test.should_return_http_header_application_json = function()
+  request = { params = function() return { json_callback = nil} end }
+  local c = Controller.new()
+  local object = { id = 1, name = 'John' }
+  local status, headers, body = c:render({ output = 'json', value = object })
+  assert( headers[1] == 'Content-Type: application/json; charset=UTF-8', headers[1] )
+end
+
+test.should_return_http_body_encoded_json = function()
+  request = { params = function() return { json_callback = nil} end }
+  local c = Controller.new()
+  local object = { id = 1, name = 'John' }
+  local status, headers, body = c:render({ output = 'json', value = object })
+  assert( body == [[{"name":"John","id":1}]], body )
+end
+
+test.should_return_with_json_callback = function()
+  request = { params = function() return { json_callback = 'mycallback'} end }
+  local c = Controller.new()
+  local object = { id = 1, name = 'John' }
+  local status, headers, body = c:render({ output = 'json', value = object })
+  assert( body == [[mycallback({"name":"John","id":1})]], body )
+end
+
 return test
