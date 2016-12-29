38e5808f632d5b106cdf8bb15519834c6a81bc4f
Alexandre Riveira <alexandre@objectdata.com.br>
2016-12-29 14:09:23 -0200

    coverage Controller

--- tests/lib/charon/Controller/helper.lua
+++ tests/lib/charon/Controller/helper.lua
@@ -1,2 +1,40 @@
+local Controller = require('charon.Controller')
 local test = {}
+
+test.before = function()
+  Controller.prefixHelpers = 'util.helpers.'
+end
+
+test.should_return_default_helper_by_metatable = function()
+  Controller.prefixHelpers = 'util.disabled.'
+  local c = Controller.new{ controller_name = 'controller' }
+  local h = c:helper()
+  Controller.prefixHelpers = 'util.helpers.'
+  assert( getmetatable(h) == require('charon.Helper') )
+end
+
+test.should_return_defaul_helper_if_exists = function()
+  local c = Controller.new{ controller_name = 'controller' }
+  local h = c:helper()
+  assert( type(h.mydefaulthelper) == 'function', type(h.mydefaulthelper) )
+end
+
+test.should_return_function_from_base_helper = function()
+  local c = Controller.new{ controller_name = 'controller' }
+  local h = c:helper()
+  assert( type(h.fileTimestamp) == 'function', type(h.mydefaulthelper) )
+end
+
+test.should_return_helper_from_controller_name = function()
+  local MyController = Class.new("MyController", Controller)
+  local c = MyController.new{ controller_name = 'my_controller' }
+  local h = c:helper()
+  -- base helper
+  assert( type(h.nl2br) == 'function', type(h.mydefaulthelper) )
+  -- default helper
+  assert( type(h.mydefaulthelper) == 'function', type(h.mydefaulthelper) )
+  -- named helper
+  assert( type(h.mycontrollerhelper) == 'function', type(h.mycontrollerhelper) )
+end
+
 return test
