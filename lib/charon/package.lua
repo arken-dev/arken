9c768572060f49358737027eb5c258b0eb4da3dd
Alexandre Riveira <alexandre@objectdata.com.br>
2017-01-02 13:55:02 -0200

    ident

--- lib/charon/package.lua
+++ lib/charon/package.lua
@@ -3,7 +3,7 @@
 -- Use of this source code is governed by a BSD-style
 -- license that can be found in the LICENSE file.
 
-package.time = os.microtime()
+package.time  = os.microtime()
 package.mixed = {}
 package.cache = {}
 
