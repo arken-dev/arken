dc1b1a668451bc67f33fcf72071f99293d0b7fe6
Alexandre Riveira <alexandre@objectdata.com.br>
2017-01-12 23:32:05 -0200

    test

--- tests/lib/charon/dispatcher/requireController.lua
+++ tests/lib/charon/dispatcher/requireController.lua
@@ -3,7 +3,8 @@ local dispatcher = require('charon.dispatcher')
 package.path = package.path .. ';util/?.lua'
 
 test.should_return_require_controller_with_camelcase = function()
-  local class = dispatcher.requireController('util')
+  local controller = dispatcher.requireController('util')
+  assert(controller.className == 'UtilController', controller.className )
 end
 
 return test
