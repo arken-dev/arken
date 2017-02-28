0f8b328d28d1ab64bd8f7f7a954d8804288e217a
Alexandre Riveira <alexandre@objectdata.com.br>
2017-02-27 22:30:39 -0300

    windows.jit

--- bootstrap/windows.jit.sh
+++ bootstrap/windows.jit.sh
@@ -1,13 +1,5 @@
 cd tmp
 rm -Rf windows
-curl http://charonplatform.org/download/windows.zip  -o windows.zip
-unzip windows.zip
-mv windows/* ../deps/
-
-curl http://luajit.org/download/LuaJIT-2.0.4.tar.gz -o tmp.tar.gz
-tar -xzvf tmp.tar.gz
-cd LuaJIT-2.0.4
-mingw32-make
-mingw32-make install PREFIX=$PWD/../luajit
-cp src/lua51.dll ../../deps/liblua.dll
-cp -pr ../luajit/include/luajit-2.0/ ../../deps/include/lua
+curl http://charonplatform.org/download/windows-jit.zip  -o windows-jit.zip
+unzip windows-jit.zip
+mv windows-jit/* ../deps/
