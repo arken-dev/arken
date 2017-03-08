afaac7371fe00cce1a814790055b35d70bc7ccb1
Alexandre Riveira <alexandre@objectdata.com.br>
2017-03-08 18:50:47 -0300

    fix tests

--- tests/lib/charon/Controller/redirect.lua
+++ tests/lib/charon/Controller/redirect.lua
@@ -1,9 +1,13 @@
 local Controller = require('charon.Controller')
 local test       = {}
+local env        = {}
+
+env.field = function()
+  return 'localhost'
+end
 
 test.should_return_http_status_headers_and_body_nil = function()
-  request = { field = function() return 'localhost' end }
-  local c = Controller.new{ controllerName = 'index' }
+  local c = Controller.new{ _env = env, controllerName = 'index' }
   local http, headers, body = c:redirect{ action = 'list' }
   assert( http == 302 )
   assert( headers[1] == "Location: http://localhost/index/list", headers[1] )
