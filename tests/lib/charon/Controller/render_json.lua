54e237be68daa3c30b18fa8749a2983eda37f7f5
Alexandre Riveira <alexandre@objectdata.com.br>
2017-03-08 18:49:24 -0300

    fix test json

--- tests/lib/charon/Controller/render_json.lua
+++ tests/lib/charon/Controller/render_json.lua
@@ -1,33 +1,43 @@
 local Controller = require 'charon.Controller'
 local test = {}
 
+-- Mock
+local env  = {}
+
+env.requestMethod = function()
+  return 'GET'
+end
+
+env.queryString = function()
+  return ''
+end
+
 test.should_return_http_status_500_if_errors = function()
-  request = { params = function() return { json_callback = nil} end }
-  local c = Controller.new()
+  local c = Controller.new{ _env = env }
   local object = { id = 1, name = 'John' }
   local status, headers, body = c:render({ output = 'json', value = object })
   assert( status == 200 )
 end
 
 test.should_return_http_header_application_json = function()
-  request = { params = function() return { json_callback = nil} end }
-  local c = Controller.new()
+  local c = Controller.new{ _env = env }
   local object = { id = 1, name = 'John' }
   local status, headers, body = c:render({ output = 'json', value = object })
   assert( headers[1] == 'Content-Type: application/json; charset=UTF-8', headers[1] )
 end
 
 test.should_return_http_body_encoded_json = function()
-  request = { params = function() return { json_callback = nil} end }
-  local c = Controller.new()
+  local c = Controller.new{ _env = env }
   local object = { id = 1, name = 'John' }
   local status, headers, body = c:render({ output = 'json', value = object })
   assert( body == [[{"name":"John","id":1}]], body )
 end
 
 test.should_return_with_json_callback = function()
-  request = { params = function() return { json_callback = 'mycallback'} end }
-  local c = Controller.new()
+  env.queryString = function()
+    return 'json_callback=mycallback'
+  end
+  local c = Controller.new{ _env = env }
   local object = { id = 1, name = 'John' }
   local status, headers, body = c:render({ output = 'json', value = object })
   assert( body == [[mycallback({"name":"John","id":1})]], body )
