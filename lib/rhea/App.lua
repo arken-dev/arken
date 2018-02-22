aeee2a803f86995b48c1b5a151b964e41ee7f602
Lazarone S. Santana <lazarone@objectdata.com.br>
2018-02-21 21:31:23 -0300

    voltando

--- lib/rhea/App.lua
+++ lib/rhea/App.lua
@@ -13,7 +13,6 @@ App.help.create = [[
 ]]
 
 function App:create()
-  local params  = self:params()
   local dirName = os.pwd() .. '/' .. params[1]
   if os.exists(dirName) then
     error(dirName .. ' exists')
