d0f35a206d6deb6625aec492dd204efda2d24ee1
Lazarone S. Santana <lazarone@objectdata.com.br>
2018-03-03 13:04:50 -0300

    layout

--- skel/app/controllers/IndexController.lua
+++ skel/app/controllers/IndexController.lua
@@ -1,6 +1,9 @@
 local IndexController = Class.new("IndexController", "Controller")
 
+IndexController.layout = "main"
+
 function IndexController:indexAction()
+  -- http://localhost:2345/
   return self:render{ output = 'html' }
 end
 
