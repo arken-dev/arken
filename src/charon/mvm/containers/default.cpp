38a1dd0277ccbda1c45424a2701df97dfc0576aa
Alexandre Riveira <alexandre@objectdata.com.br>
2018-03-26 08:59:40 -0300

    default

---
+++ src/charon/mvm/containers/default.cpp
@@ -0,0 +1,54 @@
+// Copyright 2016 The Charon Platform Authors.
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include <charon/base>
+#include <charon/mvm>
+#include <iostream>
+
+using namespace charon;
+
+static mvm::data *  s_container[30];
+static mvm::data ** s_pointer;
+
+void mvm::container::init()
+{
+  std::cout << "init...";
+  s_pointer = s_container;
+}
+
+void mvm::container::push(mvm::data * data)
+{
+  //if( s_count > s_capacity ) {
+    // realloc memory
+  //}
+  //s_container[s_count++] = data;
+  *s_pointer = data;
+   s_pointer++;
+}
+
+void mvm::container::back(mvm::data * data)
+{
+  /*
+  int i = 0;
+  mvm::data * tmp1 = data;
+  mvm::data * tmp2;
+  while( i < s_count ) {
+    tmp2 = s_container[i];    
+    s_container[i] = tmp1;
+    tmp1 = tmp2;
+  }
+  */
+}
+
+mvm::data * mvm::container::pop()
+{
+  s_pointer--;
+  return *s_pointer;
+}
+
+bool mvm::container::empty()
+{
+  return s_pointer == s_container;
+}
