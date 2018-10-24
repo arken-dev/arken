5f42a9933e2f2abafa44bf73d548d2bb22215a52
Alexandre Riveira <alexandre@objectdata.com.br>
2018-10-24 20:31:11 -0300

    log

---
+++ include/charon/log.h
@@ -0,0 +1,49 @@
+// Copyright 2016 The Charon Platform Authors.
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#ifndef _CHARON_LOG_
+#define _CHARON_LOG_
+
+#include <fstream>
+#include <mutex>
+#include <map>
+
+namespace charon
+{
+
+  class Log
+  {
+
+    private:
+    std::string m_fileName;
+    std::string * m_container;
+    std::mutex  * m_mutex;
+    std::mutex  * m_dump;
+
+    static std::map<std::string, int>           m_references;
+    static std::map<std::string, std::string *> m_containers;
+    static std::map<std::string, std::mutex  *> m_mutexes;
+    static std::map<std::string, std::mutex  *> m_dumps;
+
+    void lock();
+    void unlock();
+
+    public:
+    Log(const char * fileName);
+    ~Log();
+    void append(const char * value);
+    void log(const char * level, const char * log);
+    void trace(const char * value);
+    void debug(const char * value);
+    void info(const char * value);
+    void warn(const char * value);
+    void error(const char * value);
+    void fatal(const char * value);
+    void dump();
+
+  };
+
+} // namespace
+#endif
---
+++ src/charon/log.cpp
@@ -0,0 +1,129 @@
+// Copyright 2016 The Charon Platform Authors.
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include <charon/base>
+#include <charon/log>
+#include <ctime>
+
+using namespace charon;
+
+std::map<std::string, int>           Log::m_references;
+std::map<std::string, std::string *> Log::m_containers;
+std::map<std::string, std::mutex  *> Log::m_mutexes;
+std::map<std::string, std::mutex  *> Log::m_dumps;
+static std::mutex m;
+
+Log::Log(const char * fileName)
+{
+  m_fileName = fileName;
+  m.lock();
+  if ( m_references.count(m_fileName) == 0 ) {
+    m_references[m_fileName]    = 0;
+    m_mutexes[m_fileName]       = new std::mutex;
+    m_dumps[m_fileName]         = new std::mutex;
+    m_containers[m_fileName]    = new std::string("");
+    //m_containers[m_fileName]->reserve(1048576000);
+
+  }
+
+  m_references[m_fileName]++;
+  m_container = m_containers[m_fileName];
+  m_mutex     = m_mutexes[m_fileName];
+  m_dump      = m_dumps[m_fileName];
+  m.unlock();
+}
+
+Log::~Log()
+{
+  m_references[m_fileName]--;
+  if( m_references[m_fileName] == 0 ) {
+    delete m_mutexes[m_fileName];
+    delete m_dumps[m_fileName];
+    delete m_containers[m_fileName];
+    m_containers.erase(m_fileName);
+    m_mutexes.erase(m_fileName);
+    m_dumps.erase(m_fileName);
+    m_references.erase(m_fileName);
+  }
+}
+
+void Log::append(const char * value)
+{
+  this->lock();
+  m_container->append(value);
+  m_container->append("\n");
+  this->unlock();
+}
+
+void Log::log(const char * level, const char * value)
+{
+  std::string log(level);
+
+  // time
+  std::time_t result = std::time(nullptr);
+  char * t = std::asctime(std::localtime(&result));
+  t[24] = 32;
+  log.append(t);
+  log.append(value);
+  this->append(log.c_str());
+}
+
+void Log::trace(const char * value)
+{
+  this->log("TRACE|", value);
+}
+
+void Log::debug(const char * value)
+{
+  this->log("DEBUG|", value);
+}
+
+void Log::info(const char * value)
+{
+  this->log("INFO|", value);
+}
+
+void Log::warn(const char * value)
+{
+  this->log("WARN|", value);
+}
+
+void Log::error(const char * value)
+{
+  this->log("ERROR|", value);
+}
+
+void Log::fatal(const char * value)
+{
+  this->log("FATAL|", value);
+}
+
+void Log::dump()
+{
+  this->lock();
+  std::string * tmp = m_container;
+  m_container = new std::string("");
+  m_containers[m_fileName]  = m_container;
+  this->unlock();
+
+  // write file
+  m_dumps[m_fileName]->lock();
+  std::ofstream file;
+  file.open (m_fileName, std::ofstream::out | std::ofstream::app);
+  file << *tmp;
+  file.close();
+  delete tmp;
+  m_dumps[m_fileName]->unlock();
+}
+
+void Log::lock()
+{
+  m_mutexes[m_fileName]->lock();
+}
+
+void Log::unlock()
+{
+  m_mutexes[m_fileName]->unlock();
+}
