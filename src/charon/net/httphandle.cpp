e101e226c8a5b7394a2ac90901b452842b3fa127
Alexandre Fresneda Riveira <alexandre@objectdata.com.br>
2018-12-22 16:36:35 -0200

    remove QByteArray

--- src/charon/net/httphandle.cpp
+++ src/charon/net/httphandle.cpp
@@ -5,6 +5,7 @@
 
 #include <lua/lua.hpp>
 #include <charon/base>
+#include <string>
 
 using namespace charon::net;
 
@@ -40,7 +41,7 @@ char * HttpHandle::sync(const char * data, int size, int * num)
     if (lua_isstring( L, -1 )) {
       result = lua_tolstring( L, -1, &len );
       buffer.append("Content-Length:");
-      buffer.append(QByteArray::number(((int)len), 10));
+      buffer.append(std::to_string(len));
       buffer.append("\r\n\r\n");
       buffer.append(result, len);
     }
@@ -68,14 +69,14 @@ char * HttpHandle::sync(const char * data, int size, int * num)
         if( lua_isuserdata( L, 3 ) ) {
           HttpBody * body = *(HttpBody **) lua_touserdata(L, 3);
           buffer.append("Content-Length:");
-          buffer.append(QByteArray::number((int)body->size(), 10));
+          buffer.append(std::to_string(body->size()));
           buffer.append("\r\n\r\n");
           buffer.append(body->read(), body->size());
           body->release();
         } else {
           result = luaL_checklstring( L, 3, &len );
           buffer.append("Content-Length:");
-          buffer.append(QByteArray::number((int)len, 10));
+          buffer.append(std::to_string(len));
           buffer.append("\r\n\r\n");
           buffer.append(result, len);
         }
