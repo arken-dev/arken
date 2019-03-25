09ddb0c6a71fbb4214471e877f6dbcd9c80c7fb7
Alexandre Fresneda Riveira <alexandre@objectdata.com.br>
2019-03-25 20:17:08 -0300

    packages

--- src/projects/charon/packages.lua
+++ src/projects/charon/packages.lua
@@ -7,15 +7,26 @@ if not os.exists(packages) then
   print(packages .. ' not exists using default')
   packages = string.format("%s/config/packages.default", CHARON_PATH)
 end
+
 for package in io.lines(packages) do
   print('package ' .. package)
   package = package:simplified()
   local name     = package:sub(1, package:indexOf(' ')-1)
   local url      = package:sub(package:indexOf(' ')+1)
-  local filename = string.format("%s/packages/%s.zip", CHARON_PATH, name)
-  local data     = os.read(url)
-  local file     = io.open(filename, "w+")
-  file:write(data)
-  file:close()
-  zip.decompress(filename, CHARON_PATH .. '/packages/' .. name)
+
+  if url:endsWith(".git") then
+    print( string.format("%s/packages/%s", CHARON_PATH, name) )
+    if os.exists(string.format("%s/packages/%s", CHARON_PATH, name)) then
+      os.execute(string.format("cd %s/packages/%s; git pull", CHARON_PATH, name))
+    else
+      os.execute(string.format("git clone %s %s/packages/%s", url, CHARON_PATH, name))
+    end
+  else
+    local filename = string.format("%s/packages/%s.zip", CHARON_PATH, name)
+    local data     = os.read(url)
+    local file     = io.open(filename, "w+")
+    file:write(data)
+    file:close()
+    zip.decompress(filename, CHARON_PATH .. '/packages/' .. name)
+  end
 end
