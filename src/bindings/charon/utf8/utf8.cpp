0d1f3a13f15e55e38a75c8d2936232bdeacc4f11
Alexandre Fresneda Riveira <alexandre@objectdata.com.br>
2019-07-02 13:35:01 -0300

    utf8 encode|decode

--- src/bindings/charon/utf8/utf8.cpp
+++ src/bindings/charon/utf8/utf8.cpp
@@ -27,6 +27,20 @@ static int charon_utf8_format( lua_State *L ) {
   return 1;
 }
 
+static int charon_utf8_decode( lua_State *L ) {
+  const char * charset = luaL_checkstring(L, 1);
+  const char * string  = luaL_checkstring(L, 2);
+  lua_pushstring( L, utf8::decode(charset, string) );
+  return 1;
+}
+
+static int charon_utf8_encode( lua_State *L ) {
+  const char * charset = luaL_checkstring(L, 1);
+  const char * string  = luaL_checkstring(L, 2);
+  lua_pushstring( L, utf8::encode(charset, string) );
+  return 1;
+}
+
 static int charon_utf8_len( lua_State *L ) {
   const char * string = luaL_checkstring(L, 1);
   lua_pushinteger( L, utf8::len(string) );
@@ -37,10 +51,12 @@ static int charon_utf8_len( lua_State *L ) {
 extern "C" {
   int luaopen_charon_utf8( lua_State *L ) {
     static const luaL_reg Map[] = {
-      {"lower", charon_utf8_lower},
-      {"upper", charon_utf8_upper},
+      {"lower",  charon_utf8_lower},
+      {"upper",  charon_utf8_upper},
       {"format", charon_utf8_format},
-      {"len",   charon_utf8_len},
+      {"len",    charon_utf8_len},
+      {"decode", charon_utf8_decode},
+      {"encode", charon_utf8_encode},
       {NULL, NULL}
     };
     luaL_newmetatable(L, "utf8");
