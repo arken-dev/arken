6219e4b87aa561913a260215afccef18e78ddcdb
Alexandre Riveira <alexandre@objectdata.com.br>
2020-01-09 14:46:11 -0300

    Controller#render_file

--- lib/charon/Controller.lua
+++ lib/charon/Controller.lua
@@ -266,6 +266,98 @@ function Controller:render_json(params)
 end
 
 -------------------------------------------------------------------------------
+-- RENDER FILE
+-------------------------------------------------------------------------------
+Controller.contetTypeHash = {
+  ["aac"]   = "audio/aac", -- Arquivo de audio AAC
+  ["abw"]   = "application/x-abiword", -- Documento AbiWord
+  ["arc"]   = "application/octet-stream", -- Archive document (multiple files embedded)
+  ["avi"]   = "video/x-msvideo", -- Arquivo de audio e vídeo  Intercalar AVI
+  ["azw"]   = "application/vnd.amazon.ebook", -- Formato eBook do Amazon Kindle
+  ["bin"]   = "application/octet-stream", -- Qualquer tipo de dados binários
+  ["bz"]    = "application/x-bzip", -- Arquivo compactado  BZip
+  ["bz2"]   = "application/x-bzip2", -- Arquivo compactado  BZip2
+  ["csh"]   = "application/x-csh", -- C-Shell script
+  ["css"]   = "text/css", -- Cascading Style Sheets (CSS)
+  ["csv"]   = "text/csv", -- Comma-separated values (CSV)
+  ["doc"]   = "application/msword", -- Microsoft Word
+  ["eot"]   = "application/vnd.ms-fontobject", -- MS Embedded OpenType fonts
+  ["epub"]  = "application/epub+zip", -- Electronic publication (EPUB)
+  ["gif"]   = "image/gif", -- Graphics Interchange Format (GIF)
+  ["htm"]   = "text/html", -- HyperText Markup Language (HTML)
+  ["html"]  = "text/html", -- HyperText Markup Language (HTML)
+  ["ico"]   = "image/x-icon", -- Icon format
+  ["ics"]   = "text/calendar", -- iCalendar format
+  ["jar"]   = "application/java-archive", -- Java Archive (JAR)
+  ["jpg"]   = "image/jpeg", -- JPEG images
+  ["jpeg"]  = "image/jpeg", -- JPEG images
+  ["js"]    = "application/javascript", -- JavaScript (ECMAScript)
+  ["json"]  = "application/json", -- JSON format
+  ["mid"]   = "audio/midi", -- Musical Instrument Digital Interface (MIDI)
+  ["midi"]  = "audio/midi", -- Musical Instrument Digital Interface (MIDI)
+  ["mpeg"]  = "video/mpeg", -- MPEG Video
+  ["mpkg"]  = "application/vnd.apple.installer+xml", -- Apple Installer Package
+  ["odp"]   = "application/vnd.oasis.opendocument.presentation", -- OpenDocument presentation document
+  ["ods"]   = "application/vnd.oasis.opendocument.spreadsheet", -- OpenDocument spreadsheet document
+  ["odt"]   = "application/vnd.oasis.opendocument.text", -- OpenDocument text document
+  ["oga"]   = "audio/ogg", -- OGG audio
+  ["ogv"]   = "video/ogg", -- OGG video
+  ["ogx"]   = "application/ogg", -- OGG
+  ["otf"]   = "font/otf", -- OpenType font
+  ["png"]   = "image/png", -- Portable Network Graphics
+  ["pdf"]   = "application/pdf", -- Adobe Portable Document Format (PDF)
+  ["ppt"]   = "application/vnd.ms-powerpoint", -- Microsoft PowerPoint
+  ["rar"]   = "application/x-rar-compressed", -- RAR archive
+  ["rtf"]   = "application/rtf", -- Rich Text Format (RTF)
+  ["sh"]    = "application/x-sh", -- Bourne shell script
+  ["svg"]   = "image/svg+xml", -- Scalable Vector Graphics (SVG)
+  ["swf"]   = "application/x-shockwave-flash", -- Small web format (SWF) or Adobe Flash document
+  ["tar"]   = "application/x-tar", -- Tape Archive (TAR)
+  ["tif"]   = "image/tiff", -- Tagged Image File Format (TIFF)
+  ["tiff"]  = "image/tiff", -- Tagged Image File Format (TIFF)
+  ["ts"]    = "application/typescript", -- Typescript file
+  ["ttf"]   = "font/ttf", -- TrueType Font
+  ["vsd"]   = "application/vnd.visio", -- Microsoft Visio
+  ["wav"]   = "audio/x-wav", -- Waveform Audio Format
+  ["weba"]  = "audio/webm", -- WEBM audio
+  ["webm"]  = "video/webm", -- WEBM video
+  ["webp"]  = "image/webp", -- WEBP image
+  ["woff"]  = "font/woff", -- Web Open Font Format (WOFF)
+  ["woff2"] = "font/woff2", -- Web Open Font Format (WOFF)
+  ["xhtml"] = "application/xhtml+xml", -- XHTML
+  ["xls"]   = "application/vnd.ms-excel", -- Microsoft Excel
+  ["xlsx"]  = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", -- Microsoft Excel
+  ["xml"]   = "application/xml", -- XML
+  ["xul"]   = "application/vnd.mozilla.xul+xml", -- XUL
+  ["zip"]   = "application/zip", -- ZIP archive
+  ["3gp"]   = "video/3gpp", -- 3GPP audio/video container
+  ["3g2"]   = "video/3gpp2", -- 3GPP2 audio/video container
+  ["7z"]    = "application/x-7z-compressed" -- 7-zip archive
+}
+
+function Controller:render_file(params)
+  local HttpBody = require('charon.net.HttpBody')
+  local code = params.code or 200
+  local path = params.path or nil
+
+  if params.view ~= nil then
+    path = string.format("%s/%s/%s", APP_PATH, self.prefixViews, params.view)
+  end
+
+  local ext  = params.ext or path:suffix(".")
+
+  if params.name == nil then
+    params.name = path:suffix("/"):prefix(".")
+  end
+
+  local headers = {}
+  table.insert(headers, string.format("Content-Type: %s", Controller.contetTypeHash[ext]))
+  table.insert(headers, string.format("Content-Disposition: attachment;filename=%s.%s", params.name, ext))
+
+  return code, headers, HttpBody.loadFile(path)
+end
+
+-------------------------------------------------------------------------------
 -- EXECUTE
 -------------------------------------------------------------------------------
 
