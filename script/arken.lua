f3950607d47b64fd11cc4e6b397c1036740aedab
Alexandre Riveira <alexandre@objectdata.com.br>
2020-09-21 18:56:01 -0300

    arken

--- script/arken.lua
+++ script/arken.lua
@@ -1,15 +1,15 @@
 local list = {'.'} --'app', 'lib', 'tests', 'util', 'importacoes', 'api'}
 for _, dir in ipairs(list) do
-  local list = os.glob(dir, '\\.cpp$|\\.lua|\\.h|\\.md|\\.txt', true)
+  local list = os.glob(dir, '\\.txt$', true)
 
   for fileName in list:each() do
 
     print(fileName)
 
     local buffer = os.read(fileName)
-    local buffer = buffer:replace('arken', 'arken')
-    local buffer = buffer:replace('ARKEN', 'ARKEN')
-    local buffer = buffer:replace('Arken', 'Arken')
+    local buffer = buffer:replace('charon', 'arken')
+    local buffer = buffer:replace('CHARON', 'ARKEN')
+    local buffer = buffer:replace('Charon', 'Arken')
 
     local file = io.open(fileName, 'w')
     file:write(buffer)
