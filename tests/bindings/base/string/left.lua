dcadeb68969377650b7f7cd69ecb2b1dbba8c79b
Alexandre Riveira <alexandre@objectdata.com.br>
2021-02-25 13:53:17 -0300

    tests

--- tests/bindings/base/string/left.lua
+++ tests/bindings/base/string/left.lua
@@ -1,14 +1,14 @@
 local test = {}
 
 test['should before string'] = function()
-  str1 = "this is a test"
-  str2 = "this is"
+  local str1 = "this is a test"
+  local str2 = "this is"
   assert( str1:left(7) == str2 )
 end
 
 test['should before string with new line'] = function()
-  str1 = "\nthis is a test"
-  str2 = "\nthis is"
+  local str1 = "\nthis is a test"
+  local str2 = "\nthis is"
   assert( str1:left(8) == str2 )
 end
 
