c15be48e856c4d5a3546b36806aab55f6a9a8ae1
Alexandre Riveira <alexandre@objectdata.com.br>
2021-06-18 20:25:00 -0300

    arken.concurrent.task.priority

---
+++ src/arken/concurrent/task/priority.cpp
@@ -0,0 +1,197 @@
+#include <arken/concurrent/task/priority.h>
+#include <arken/os.h>
+#include <cstdio>
+#include <lua/lua.hpp>
+#include <lua/json/lock.h>
+#include <arken/mvm>
+
+
+namespace arken {
+namespace concurrent {
+namespace task {
+
+using mvm = arken::mvm;
+
+std::mutex priority::s_mutex;
+std::atomic<int> priority::s_max(1);
+std::atomic<int> priority::s_actives(0);
+
+priority::priority()
+{
+  priority::s_actives++;
+}
+
+priority::~priority()
+{
+  priority::s_actives--;
+}
+
+std::priority_queue<priority::node, std::vector<priority::node>, priority::node> & priority::priority_queue()
+{
+  static std::priority_queue<priority::node, std::vector<priority::node>, priority::node> s_priority_queue;
+  return s_priority_queue;
+}
+
+void priority::run()
+{
+  while (true) {
+    priority::node node = dequeue();
+    if( !node ) {
+      break;
+    }
+    node.run();
+  }
+}
+
+bool priority::release()
+{
+  return true;
+}
+
+priority::node priority::start(const char * fileName, const char * params, int priority, bool purge)
+{
+  std::unique_lock<std::mutex> lck(priority::s_mutex);
+  priority::node node = priority::node(fileName, params, priority, purge);
+  priority::push( node );
+
+  if(priority::s_actives < priority::s_max) {
+    mvm::concurrent( new arken::concurrent::task::priority() );
+  }
+
+  return node;
+}
+
+priority::node::node()
+{}
+
+priority::node::node(const node &obj)
+{
+  m_uuid      = obj.m_uuid;
+  m_fileName  = obj.m_fileName;
+  m_params    = obj.m_params;
+  m_priority  = obj.m_priority;
+  m_microtime = obj.m_microtime;
+  m_shared    = obj.m_shared;
+}
+
+priority::node::node(const char * fileName, const char * params, int priority, bool purge)
+{
+  m_fileName  = fileName;
+  m_params    = params;
+  m_priority  = priority;
+  m_purge     = purge;
+  m_uuid      = os::uuid();
+  m_microtime = os::microtime();
+
+}
+
+bool priority::node::operator()(const priority::node &n1, const priority::node &n2)
+{
+  if( n1.m_priority == n2.m_priority ) {
+    return n1.m_microtime > n2.m_microtime;
+  }
+
+  return n1.m_priority < n2.m_priority;
+}
+
+void priority::node::run()
+{
+  int rv;
+  arken::instance i = mvm::instance(m_purge);
+  lua_State * L = i.state();
+  lua_settop(L, 0);
+
+  lua_getglobal(L,  "require");
+  lua_pushstring(L, "arken.concurrent.task.priority");
+  rv = lua_pcall(L, 1, 0, 0);
+  if (rv) {
+    fprintf(stderr, "%s\n", lua_tostring(L, -1));
+  }
+
+  if( m_fileName.endsWith(".lua") ) {
+    lua_getglobal(L, "dofile");
+    lua_pushstring(L, m_fileName);
+    rv = lua_pcall(L, 1, 1, 0);
+    if (rv) {
+      fprintf(stderr, "%s\n", lua_tostring(L, -1));
+    }
+  } else {
+    lua_getglobal(L, "require");
+    lua_pushstring(L, m_fileName);
+    rv = lua_pcall(L, 1, 1, 0);
+    if (rv) {
+      fprintf(stderr, "%s\n", lua_tostring(L, -1));
+    }
+  }
+
+  //lua_pushlstring(L,  m_uuid, 37);
+  auto ptr = static_cast<priority::node **>(lua_newuserdata(L, sizeof(priority::node*)));
+  *ptr = new priority::node(*this);
+  luaL_getmetatable(L, "arken.concurrent.task.priority.node.metatable");
+  lua_setmetatable(L, -2);
+
+  json_lock_decode(L, m_params);
+
+  rv = lua_pcall(L, 2, 0, 0); // alterar para envio de 1
+  if (rv) {
+    fprintf(stderr, "erro no inicio: %s\n", lua_tostring(L, -1));
+  }
+
+  // GC
+  if( m_purge ) {
+    i.release();
+    lua_close(L);
+  } else {
+    lua_gc(L, LUA_GCCOLLECT, 0);
+  }
+}
+
+void priority::push(const priority::node & node)
+{
+  priority::priority_queue().push( node );
+}
+
+priority::node priority::dequeue()
+{
+  std::unique_lock<std::mutex> lck(priority::s_mutex);
+  if (priority::priority_queue().empty()) {
+    return priority::node();
+  }
+
+  priority::node n = priority::priority_queue().top();
+  priority::priority_queue().pop();
+  return n;
+}
+
+void priority::wait()
+{
+  mvm::wait();
+}
+
+string priority::node::uuid()
+{
+  return m_uuid;
+}
+
+int priority::node::priority()
+{
+  return m_priority;
+}
+
+double priority::node::microtime()
+{
+  return m_microtime;
+}
+
+priority::node::operator bool() const {
+  return m_microtime > 0;
+}
+
+Shared priority::node::shared()
+{
+  return m_shared;
+}
+
+}  // namespace task
+}  // namespace concurrent
+}  // namespace arken
---
+++ src/bindings/arken/concurrent/task/priority/priority.cpp
@@ -0,0 +1,173 @@
+// Copyright 2016 The Arken Platform Authors.
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include <lua/lua.hpp>
+#include <arken/base>
+#include <arken/concurrent/task/priority.h>
+
+using priority  = arken::concurrent::task::priority;
+using Shared = arken::concurrent::Shared;
+
+char * json_lock_encode(lua_State *L);
+void   json_lock_decode(lua_State *L, const char * data);
+
+priority *
+checkNaiad( lua_State *L ) {
+  return *(priority **) luaL_checkudata(L, 1, "arken.concurrent.task.priority.metatable");
+}
+
+priority::node *
+checkNode( lua_State *L ) {
+  return *(priority::node **) luaL_checkudata(L, 1, "arken.concurrent.task.priority.node.metatable");
+}
+
+
+//-----------------------------------------------------------------------------
+// Class Methods
+//-----------------------------------------------------------------------------
+
+static int
+arken_priority_start(lua_State *L) {
+  bool release  = false;
+  int  priority = 0;
+  char * params = nullptr;
+  const char * fileName = luaL_checkstring(L, 1);
+
+  if(lua_gettop(L) == 4) { // number of arguments
+    release = lua_toboolean(L, 4);
+  }
+
+  if(lua_gettop(L) >= 3) { // number of arguments
+    priority = lua_tointeger(L, 3);
+  }
+
+  if(lua_gettop(L) == 1) { /* número de argumentos */
+    params = new char[3]{'{','}','\0'};
+  } else {
+    lua_settop(L, 2);
+    params = json_lock_encode(L);
+  }
+
+  priority::node node = priority::start( fileName, params, priority, release );
+  priority::node **ptr = (priority::node **)lua_newuserdata(L, sizeof(priority::node*));
+  *ptr = new priority::node(node);
+  luaL_getmetatable(L, "arken.concurrent.task.priority.node.metatable");
+  lua_setmetatable(L, -2);
+
+  delete params;
+
+  return 1;
+}
+
+static int
+arken_priority_wait(lua_State *L) {
+  priority::wait();
+  return 0;
+}
+
+static int
+arken_priority_max(lua_State *L) {
+  int max = luaL_checkinteger(L, 1);
+  priority::s_max = max;
+  return 0;
+}
+
+static int
+arken_priority_actives(lua_State *L) {
+  lua_pushinteger(L, priority::s_actives);
+  return 1;
+}
+
+
+static const luaL_reg NaiadClassMethods[] = {
+  {"start",   arken_priority_start},
+  {"wait",    arken_priority_wait},
+  {"max",     arken_priority_max},
+  {"actives", arken_priority_actives},
+  {NULL, NULL}
+};
+
+void static
+registerNaiadClassMethods( lua_State *L ) {
+  luaL_newmetatable(L, "arken.concurrent.task.priority");
+  luaL_register(L, NULL, NaiadClassMethods);
+  lua_pushvalue(L, -1);
+  lua_setfield(L, -1, "__index");
+}
+
+static int
+arken_concurrent_channel_node_instance_method_uuid( lua_State *L ) {
+  priority::node * node = checkNode( L );
+  lua_pushlstring(L, node->uuid(), 37);
+  return 1;
+}
+
+static int
+arken_concurrent_channel_node_instance_method_priority( lua_State *L ) {
+  priority::node * node = checkNode( L );
+  lua_pushinteger(L, node->priority());
+  return 1;
+}
+
+static int
+arken_concurrent_channel_node_instance_method_microtime( lua_State *L ) {
+  priority::node * node = checkNode( L );
+  lua_pushnumber(L, node->microtime());
+  return 1;
+}
+
+static int
+arken_concurrent_channel_node_instance_method_shared( lua_State *L ) {
+  priority::node * node = checkNode( L );
+  int rv;
+  lua_getglobal(L, "require");
+  lua_pushstring(L, "arken.concurrent.Shared");
+  rv = lua_pcall(L, 1, 0, 0);
+  if (rv) {
+    fprintf(stderr, "%s\n", lua_tostring(L, -1));
+  }
+
+  Shared **ptr = (Shared **)lua_newuserdata(L, sizeof(Shared*));
+  *ptr = new Shared(node->shared());
+  luaL_getmetatable(L, "arken.concurrent.Shared.metatable");
+  lua_setmetatable(L, -2);
+
+  return 1;
+}
+
+static int
+arken_concurrent_channel_node_instance_method_destruct( lua_State *L ) {
+  priority::node * node = checkNode( L );
+  delete node;
+  return 0;
+}
+
+static const
+luaL_reg NaiadNodeInstanceMethods[] = {
+  {"uuid",      arken_concurrent_channel_node_instance_method_uuid},
+  {"priority",  arken_concurrent_channel_node_instance_method_priority},
+  {"microtime", arken_concurrent_channel_node_instance_method_microtime},
+  {"shared",    arken_concurrent_channel_node_instance_method_shared},
+  {"__gc",      arken_concurrent_channel_node_instance_method_destruct},
+  {NULL, NULL}
+};
+
+void static
+registerNodeInstanceMethods( lua_State *L ) {
+  luaL_newmetatable(L, "arken.concurrent.task.priority.node.metatable");
+  luaL_register(L, NULL, NaiadNodeInstanceMethods);
+  lua_pushvalue(L, -1);
+  lua_setfield(L, -1, "__index");
+}
+
+
+extern "C" {
+  int
+  luaopen_arken_concurrent_task_priority( lua_State *L ) {
+    registerNodeInstanceMethods(L);
+    registerNaiadClassMethods(L);
+    return 1;
+  }
+}
