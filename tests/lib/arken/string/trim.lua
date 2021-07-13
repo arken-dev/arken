08260bd98b54dcc0d9ac32753fe25181d97e234c
Alexandre Riveira <alexandre@objectdata.com.br>
2021-07-13 18:40:46 -0300

    string tests refactory

--- tests/lib/arken/string/trim.lua
+++ tests/lib/arken/string/trim.lua
@@ -2,68 +2,68 @@ local test = {}
 
 test['should remove white spaces'] = function()
   local str1 = "   this is a test   "
-  local str2 = "   this is a test"
-  assert( str1:rightTrimmed() == str2 )
+  local str2 = "this is a test"
+  assert( str1:trim() == str2, str1:trim() )
 end
 
 test['should remove left white spaces'] = function()
   local str1 = "   this is a test"
-  local str2 = "   this is a test"
-  assert( str1:rightTrimmed() == str2 )
+  local str2 = "this is a test"
+  assert( str1:trim() == str2 )
 end
 
 test['should remove right white spaces'] = function()
   local str1 = "this is a test      "
   local str2 = "this is a test"
-  assert( str1:rightTrimmed() == str2 )
+  assert( str1:trim() == str2 )
 end
 
 test['should remove new lines and white spaces'] = function()
   local str1 = "\n \nthis is a test\n \n"
-  local str2 = "\n \nthis is a test"
-  assert( str1:rightTrimmed() == str2 )
+  local str2 = "this is a test"
+  assert( str1:trim() == str2 )
 end
 
 test['should empty string for new line char'] = function()
   local str1 = "\n"
   local str2 = ""
-  assert( str1:rightTrimmed() == str2, str1 )
+  assert( str1:trim() == str2 )
 end
 
 test['should empty string for return and new line chars'] = function()
   local str1 = "\r\n"
   local str2 = ""
-  assert( str1:rightTrimmed() == str2 )
+  assert( str1:trim() == str2 )
 end
 
 test['should empty string for empty string'] = function()
   local str1 = ""
   local str2 = ""
-  assert( str1:rightTrimmed() == str2 )
+  assert( str1:trim() == str2 )
 end
 
 test['should string with one char'] = function()
   local str1 = "("
   local str2 = "("
-  assert( str1:rightTrimmed() == str2, str1:rightTrimmed() )
+  assert( str1:trim() == str2, str1:trim() )
 end
 
 test['should string with one char with space right'] = function()
   local str1 = "( "
   local str2 = "("
-  assert( str1:rightTrimmed() == str2, str1:rightTrimmed() )
+  assert( str1:trim() == str2, str1:trim() )
 end
 
 test['should string with one char with space left'] = function()
   local str1 = " ("
-  local str2 = " ("
-  assert( str1:rightTrimmed() == str2, str1:rightTrimmed() )
+  local str2 = "("
+  assert( str1:trim() == str2, str1:trim() )
 end
 
 test['should string with one char with space spaces'] = function()
   local str1 = " ("
-  local str2 = " ("
-  assert( str1:rightTrimmed() == str2, str1:rightTrimmed() )
+  local str2 = "("
+  assert( str1:trim() == str2, str1:trim() )
 end
 
 return test
--- tests/lib/arken/string/trimLeft.lua
+++ tests/lib/arken/string/trimLeft.lua
@@ -3,61 +3,61 @@ local test = {}
 test['should remove white spaces'] = function()
   local str1 = "   this is a test   "
   local str2 = "this is a test   "
-  assert( str1:leftTrimmed() == str2 )
+  assert( str1:trimLeft() == str2 )
 end
 
 test['should remove left white spaces'] = function()
   local str1 = "this is a test     "
   local str2 = "this is a test     "
-  assert( str1:leftTrimmed() == str2 )
+  assert( str1:trimLeft() == str2 )
 end
 
 test['should remove left white spaces'] = function()
   local str1 = "     this is a test"
   local str2 = "this is a test"
-  assert( str1:leftTrimmed() == str2 )
+  assert( str1:trimLeft() == str2 )
 end
 
 test['should remove new lines and white spaces'] = function()
   local str1 = "\n \nthis is a test\n \n"
   local str2 = "this is a test\n \n"
-  assert( str1:leftTrimmed() == str2 )
+  assert( str1:trimLeft() == str2 )
 end
 
 test['should empty string for new line char'] = function()
   local str1 = "\n"
   local str2 = ""
-  assert( str1:leftTrimmed() == str2, str1 )
+  assert( str1:trimLeft() == str2, str1 )
 end
 
 test['should empty string for return and new line chars'] = function()
   local str1 = "\r\n"
   local str2 = ""
-  assert( str1:leftTrimmed() == str2 )
+  assert( str1:trimLeft() == str2 )
 end
 
 test['should empty string for empty string'] = function()
   local str1 = ""
   local str2 = ""
-  assert( str1:leftTrimmed() == str2 )
+  assert( str1:trimLeft() == str2 )
 end
 
 test['should string with one char'] = function()
   local str1 = "("
   local str2 = "("
-  assert( str1:leftTrimmed() == str2, str1:leftTrimmed() )
+  assert( str1:trimLeft() == str2, str1:trimLeft() )
 end
 
 test['should string with one char with space left'] = function()
   local str1 = " ("
   local str2 = "("
-  assert( str1:leftTrimmed() == str2, str1:leftTrimmed() )
+  assert( str1:trimLeft() == str2, str1:trimLeft() )
 end
 
 test['should string with one char with space left'] = function()
   local str1 = "( "
   local str2 = "( "
-  assert( str1:leftTrimmed() == str2, str1:leftTrimmed() )
+  assert( str1:trimLeft() == str2, str1:trimLeft() )
 end
 
 return test
