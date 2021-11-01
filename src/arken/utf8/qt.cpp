463ffb42f622c5c8d20ae13b8383735d47d148f1
Alexandre F Riveira <alexandre@objectdata.com.br>
2021-11-01 16:28:00 -0300

    remove debug

--- src/arken/utf8/qt.cpp
+++ src/arken/utf8/qt.cpp
@@ -89,7 +89,6 @@ char * utf8::sanitize(const char * string)
   int i = 0, j = 0;
   while (string[i]) {
     if ( string[i] == -61 ) {
-      //std::cout << "dentro de utf8" << std::endl;
       if ( string[i + 1] < 0 && string[i + 1] != -61 ) {
         char * tmp = new char[3]();
         tmp[0] = string[i];
@@ -98,12 +97,10 @@ char * utf8::sanitize(const char * string)
         tmp[1] = string[i];
         result[j] = string[i];
         j++; i++;
-        //std::cout << "achei duas" << tmp << std::endl;
       } else {
         i++;
       }
     } else {
-      //std::cout << "caracter não utf8:" << (int) string[i] << " char " << string[i] << std::endl;
       if( string[i] > 0 ) {
         result[j] = string[i];
         j++; i++;
@@ -112,7 +109,6 @@ char * utf8::sanitize(const char * string)
       }
     }
   }
-  //std::cout << "tamanho de j " << j << std::endl;
   result[j] = '\0';
   return result;
 }
