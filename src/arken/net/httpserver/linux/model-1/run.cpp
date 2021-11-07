20e0e70f9cca7aebc754da45be4157f94feac95e
Alexandre F Riveira <alexandre@objectdata.com.br>
2021-11-07 17:34:44 -0300

    fix arken.net.HttpServer#linux

--- src/arken/net/httpserver/linux/model-1/run.cpp
+++ src/arken/net/httpserver/linux/model-1/run.cpp
@@ -4,7 +4,6 @@
 // license that can be found in the LICENSE file.
 
 #include <arken/net/httpserver.h>
-#include <arken/net/config.h>
 #include <arken/mvm.h>
 #include <string>
 #include "EventLoop.h"
