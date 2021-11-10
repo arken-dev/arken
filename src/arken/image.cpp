fc33df43b2e7d9473ad9c56df210606f181006da
Alexandre F Riveira <alexandre@objectdata.com.br>
2021-11-10 08:41:37 -0300

    image-magick

--- src/arken/image.cpp
+++ src/arken/image.cpp
@@ -1,10 +1,32 @@
 #include <arken/image.h>
+#include <wand/magick_wand.h>
 
 namespace arken {
 
 Image::Image(const char * path)
 {
   m_path = path;
+}
+
+void Image::save(const char * path, int quality)
+{
+  MagickWand *mw = NULL;
+
+  MagickWandGenesis();
+
+  /* Create a wand */
+  mw = NewMagickWand();
+
+  /* Read the input image */
+  MagickReadImage(mw, m_path);
+
+  /* write it */
+  MagickWriteImage(mw, path);
+
+  /* Tidy up */
+  if(mw) mw = DestroyMagickWand(mw);
+
+  MagickWandTerminus();
 }
 
 } // namespace
