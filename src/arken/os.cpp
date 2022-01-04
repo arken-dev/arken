770c7f4e3ccf4212c6442dfbcc687f05ca25a4a8
Alexandre Fresneda Riveira <alexandre@objectdata.com.br>
2022-01-04 09:53:39 -0300

    fix CYGWIN types

--- src/arken/os.cpp
+++ src/arken/os.cpp
@@ -3,7 +3,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-#include <arken/types.h>
 #include <arken/string.h>
 #include <stdlib.h>
 #include <string.h>
