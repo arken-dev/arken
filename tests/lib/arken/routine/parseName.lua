98f0b3101a82bd6136fb193baffe33b7d0647ac0
Alexandre F Riveira <alexandre@objectdata.com.br>
2022-04-30 18:11:28 -0300

    rhea => routine tests

--- tests/lib/arken/routine/parseName.lua
+++ tests/lib/arken/routine/parseName.lua
@@ -1,26 +1,26 @@
-local rhea = require 'rhea'
+local routine = require 'routine'
 local test = {}
 
 test['parse simple name'] = function()
-  local name, method = rhea.parseName('migrate:run')
+  local name, method = routine.parseName('migrate:run')
   assert( name == 'Migrate', name )
   assert( method == 'run' )
 end
 
 test['parse name with camelcase'] = function()
-  local name, method = rhea.parseName('my_task:run')
+  local name, method = routine.parseName('my_task:run')
   assert( name == 'MyTask', name )
   assert( method == 'run' )
 end
 
 test['simple name with namespace'] = function()
-  local name, method = rhea.parseName('db.migrate:run')
+  local name, method = routine.parseName('db.migrate:run')
   assert( name == 'db.Migrate', name )
   assert( method == 'run' )
 end
 
 test['parse camel case name with large namespace'] = function()
-  local name, method = rhea.parseName('namespace1.namespace2.namespace3.my_task:run')
+  local name, method = routine.parseName('namespace1.namespace2.namespace3.my_task:run')
   assert( name == 'namespace1.namespace2.namespace3.MyTask', name )
   assert( method == 'run' )
 end
