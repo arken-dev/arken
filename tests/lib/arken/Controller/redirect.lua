9ef59c91e7cc1728e296151573f2bc1be318dde4
Alexandre F Riveira <alexandre@objectdata.com.br>
2022-05-06 18:30:27 -0300

    fix test

--- tests/lib/arken/Controller/redirect.lua
+++ tests/lib/arken/Controller/redirect.lua
@@ -2,16 +2,21 @@ local Controller = require('arken.Controller')
 local test       = {}
 local env        = {}
 
-env.field = function()
-  return 'localhost'
+env.field = function(self, value)
+  if value == "Host" then
+    return 'localhost'
+  end
+  if value == "X-Forwarded-Proto" then
+    return 'http'
+  end
 end
 
 test.should_return_http_status_headers_and_body_nil = function()
   local c = Controller.new{ _env = env, controllerName = 'index' }
   local http, headers, body = c:redirect{ action = 'list' }
   assert( http == 302 )
-  assert( headers[1] == "Location: http://localhost/index/list", headers[1] )
-  assert( body == nil )
+  assert( headers[1] == "Location: http://localhost/app/index/list", headers[1] )
+  assert( body == "" )
 end
 
 return test
