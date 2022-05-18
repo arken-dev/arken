705cb37911b95073cc9a9dd0d8333ca40cfa6dca
Alexandre F Riveira <alexandre@objectdata.com.br>
2022-05-18 16:43:23 -0300

    tests arken.Helper

--- tests/lib/arken/template/debug.lua
+++ tests/lib/arken/template/debug.lua
@@ -5,7 +5,7 @@ test.should_source_with_line_number = function()
   template.execute("util/template/example1.tpl", {})
   local result = template.debug("util/template/example1.tpl", {})
   local value  = os.read("util/template/example1.debug")
-  assert( result == value, value)
+  assert( result == value )
 end
 
 test.should_return_error_if_file_not_exists = function()
