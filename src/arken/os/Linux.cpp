fd7aa57b17d89710c80a66fb88f40bfc7b732bfe
Alexandre F Riveira <alexandre@objectdata.com.br>
2022-05-26 08:20:49 -0300

    os.mem review

--- src/arken/os/Linux.cpp
+++ src/arken/os/Linux.cpp
@@ -18,11 +18,34 @@ string os::executablePath()
   return os::target("/proc/self/exe");
 }
 
+// https://gist.github.com/gbmhunter/00c57b55e2616cd8e1f21f77b79e59fc
+
+size_t parseLine(char *line) {
+  // This assumes that a digit will be found and the line ends in " Kb".
+  int i = strlen(line);
+  const char *p = line;
+  while (*p < '0' || *p > '9') p++;
+    line[i - 3] = '\0';
+  i = atoi(p);
+  return i;
+}
+
 size_t os::mem()
 {
-  struct rusage rusage;
-  getrusage( RUSAGE_SELF, &rusage );
-  return (size_t)(rusage.ru_maxrss);
+  //struct rusage rusage;
+  //getrusage( RUSAGE_THREAD, &rusage );
+  //return (size_t)(rusage.ru_maxrss);
+  size_t result{0};
+  FILE *file = fopen("/proc/self/status", "r");
+  char line[128];
+  while (fgets(line, 128, file) != NULL) {
+    if (strncmp(line, "VmRSS:", 6) == 0) {
+      result = static_cast<size_t>(parseLine(line));
+      break;
+    }
+  }
+  fclose(file);
+  return result;
 }
 
 const char * os::name()
