9cddcc35c866e4c8caae6c71d64684ce91090784
Alexandre F Riveira <alexandre@objectdata.com.br>
2022-06-17 10:23:54 -0300

    examples

--- examples/arken.concurrent.shared/teste-2.lua
+++ examples/arken.concurrent.shared/teste-2.lua
@@ -1,6 +1,6 @@
 local mvm  = require('arken.mvm')
 local task = require('arken.concurrent.task')
-local tsk  = task.start('outro.lua', {}, false)
+local tsk  = task.start(os.pwd() .. '/examples/arken.concurrent.shared/outro.lua', {}, false)
 
 mvm.wait()
 
