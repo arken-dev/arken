dd5545213aa6706d1093a4e4653c2c1282a32670
Alexandre F Riveira <alexandre@objectdata.com.br>
2022-06-22 20:04:54 -0300

    path

--- examples/arken.mvm.Shared/example-3.lua
+++ examples/arken.mvm.Shared/example-3.lua
@@ -1,6 +1,6 @@
 local mvm  = require('arken.mvm')
 local task = require('arken.concurrent.task')
-local tsk  = task.start(os.pwd() .. '/examples/arken.concurrent.shared/example-3-node.lua', {}, false)
+local tsk  = task.start(os.pwd() .. '/examples/arken.mvm.Shared/example-3-node.lua', {}, false)
 
 mvm.wait()
 
