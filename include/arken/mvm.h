4f8ee6ecf252d72d95f890ca0da3f8cf3e5271af
Alexandre F Riveira <alexandre@objectdata.com.br>
2022-06-22 10:01:09 -0300

    mvm

--- include/arken/mvm.h
+++ include/arken/mvm.h
@@ -8,7 +8,6 @@
 
 #include <lua/lua.hpp>
 #include <arken/string.h>
-#include <arken/shared.h>
 #include <arken/concurrent/base.h>
 #include <arken/concurrent/core.h>
 #include <arken/os.h>
@@ -22,14 +21,91 @@
 #include <cstring>
 #include <iostream>
 #include <atomic>
+#include <memory>
 
 namespace arken {
 
 class mvm {
   using string = arken::string;
-  using Shared = arken::Shared;
 
   public:
+
+  //---------------------------------------------------------------------------
+  // SHARED
+  //---------------------------------------------------------------------------
+
+  class Shared
+  {
+
+    public:
+
+    class data
+    {
+      friend class Shared;
+      /**
+       * 0 = undefined
+       * 1 = bool
+       * 2 = number
+       * 3 = string
+       */
+      short  m_flag;
+      bool   m_bool;
+      double m_number;
+      string m_string;
+
+      public:
+      short  flag();
+      bool   getBool();
+      double getNumber();
+      string getString();
+    };
+
+    using string = arken::string;
+
+    private:
+
+    std::shared_ptr<string> m_info;
+    std::shared_ptr<std::unordered_map<string, data>> m_map;
+    std::shared_ptr<std::mutex> m_mutex;
+
+    public:
+
+    void info(string info);
+    string info();
+
+    // flag
+    short flag(string key);
+    data get(string key);
+
+    // NUMBER
+    double getNumber(string key);
+    void   setNumber(string key, double value);
+    double increment(string key, double value);
+
+    // STRING
+    string getString(string key);
+    void   setString(string key, string value);
+    string append(string key, string value);
+    string prepend(string key, string value);
+
+    // BOOL
+    bool getBool(string key);
+    void setBool(string key, bool value);
+    bool toggle(string key);
+
+    // PUT METHODS
+    void put(string key, string value);
+    void put(string key, double value);
+    void put(string key, bool   value);
+
+    Shared();
+    Shared(const Shared & obj);
+    static Shared & global();
+  };
+
+  //---------------------------------------------------------------------------
+  // DATA
+  //---------------------------------------------------------------------------
 
   class data {
 
@@ -53,6 +129,10 @@ class mvm {
     Shared      shared();
   };
 
+  //---------------------------------------------------------------------------
+  // INSTANCE
+  //---------------------------------------------------------------------------
+
   class instance {
     mvm::data * m_data;
 
@@ -63,6 +143,10 @@ class mvm {
     void release();
     void swap(arken::concurrent::Shared shared);
   };
+
+  //---------------------------------------------------------------------------
+  // CONTAINER
+  //---------------------------------------------------------------------------
 
   class container {
 
@@ -77,6 +161,10 @@ class mvm {
     public:
     static void push(mvm::data *);
   };
+
+  //---------------------------------------------------------------------------
+  // MVM - attributes and methods
+  //---------------------------------------------------------------------------
 
   static std::atomic<double>   s_uptime;
   static std::atomic<uint32_t> s_gc;
--- src/arken/mvm.cpp
+++ src/arken/mvm.cpp
@@ -524,4 +524,204 @@ char * mvm::setlocale(string locale)
   return std::setlocale(LC_ALL, locale);
 }
 
+//-----------------------------------------------------------------------------
+// SHARED
+//-----------------------------------------------------------------------------
+
+mvm::Shared::Shared()
+{
+  m_info  = std::shared_ptr<string>(new string);
+  m_map   = std::shared_ptr<std::unordered_map<string, data>>(new std::unordered_map<string, data>);
+  m_mutex = std::shared_ptr<std::mutex>(new std::mutex);
+};
+
+mvm::Shared::Shared(const Shared & obj)
+{
+  m_info  = obj.m_info;
+  m_map   = obj.m_map;
+  m_mutex = obj.m_mutex;
+}
+
+void mvm::Shared::info(string info)
+{
+  std::unique_lock<std::mutex> lck(*m_mutex);
+  *m_info = info;
+}
+
+string mvm::Shared::info()
+{
+  std::unique_lock<std::mutex> lck(*m_mutex);
+  return *m_info;
+}
+
+// NUMBER
+double mvm::Shared::getNumber(string key)
+{
+  std::unique_lock<std::mutex> lck(*m_mutex);
+  if( m_map->count(key) ) {
+    return m_map->at(key).m_number;
+  } else {
+    return 0;
+  }
+}
+
+void mvm::Shared::setNumber(string key, double value)
+{
+  std::unique_lock<std::mutex> lck(*m_mutex);
+  (*m_map)[key].m_number = value;
+  (*m_map)[key].m_flag   = 2;
+}
+
+double mvm::Shared::increment(string key, double value)
+{
+  std::unique_lock<std::mutex> lck(*m_mutex);
+
+  if( m_map->count(key) ) {
+    value += m_map->at(key).m_number;
+    (*m_map)[key].m_number = value;
+  } else {
+    (*m_map)[key].m_number = value;
+  }
+
+  return value;
+}
+
+// STRING
+string mvm::Shared::getString(string key)
+{
+  std::unique_lock<std::mutex> lck(*m_mutex);
+  if( m_map->count(key) ) {
+    return m_map->at(key).m_string;
+  } else {
+    return {};
+  }
+}
+
+void mvm::Shared::setString(string key, string value)
+{
+  std::unique_lock<std::mutex> lck(*m_mutex);
+  (*m_map)[key].m_string = value;
+  (*m_map)[key].m_flag   = 3;
+}
+
+string mvm::Shared::append(string key, string value)
+{
+  std::unique_lock<std::mutex> lck(*m_mutex);
+  if( m_map->count(key) ) {
+    string v = std::move(m_map->at(key).m_string);
+    v.append(value);
+    (*m_map)[key].m_string = std::move(v);
+  } else {
+    (*m_map)[key].m_string = value;
+  }
+
+  return value;
+}
+
+string mvm::Shared::prepend(string key, string value)
+{
+  std::unique_lock<std::mutex> lck(*m_mutex);
+  if( m_map->count(key) ) {
+    string v = std::move(m_map->at(key).m_string);
+    v.prepend(value);
+    (*m_map)[key].m_string = std::move(v);
+  } else {
+    (*m_map)[key].m_string = value;
+  }
+
+  return value;
+}
+
+// BOOL
+bool mvm::Shared::getBool(string key)
+{
+  std::unique_lock<std::mutex> lck(*m_mutex);
+  if( m_map->count(key) ) {
+    return m_map->at(key).m_bool;
+  } else {
+    return false;
+  }
+
+  return true;
+}
+
+void mvm::Shared::setBool(string key, bool value)
+{
+  std::unique_lock<std::mutex> lck(*m_mutex);
+  (*m_map)[key].m_bool = value;
+  (*m_map)[key].m_flag = 1;
+}
+
+bool mvm::Shared::toggle(string key)
+{
+  std::unique_lock<std::mutex> lck(*m_mutex);
+  bool result = false;
+  if( m_map->count(key) ) {
+    result = ! m_map->at(key).m_bool;
+    (*m_map)[key].m_bool = result;
+  }
+  return result;
+}
+
+Shared & mvm::Shared::global()
+{
+  static Shared instance;
+  return instance;
+}
+
+void mvm::Shared::put(string key, string value)
+{
+  this->setString(key, value);
+}
+
+void mvm::Shared::put(string key, double value)
+{
+  this->setNumber(key, value);
+}
+
+void mvm::Shared::put(string key, bool value)
+{
+  this->setBool(key, value);
+}
+
+short mvm::Shared::flag(string key)
+{
+  std::unique_lock<std::mutex> lck(*m_mutex);
+  if( m_map->count(key) ) {
+    return m_map->at(key).m_flag;
+  } else {
+    return 0;
+  }
+}
+
+mvm::Shared::data mvm::Shared::get(string key)
+{
+  std::unique_lock<std::mutex> lck(*m_mutex);
+  if( m_map->count(key) ) {
+    return m_map->at(key);
+  } else {
+    return {};
+  }
+}
+
+short mvm::Shared::data::flag()
+{
+  return m_flag;
+}
+
+bool mvm::Shared::data::getBool()
+{
+  return m_bool;
+}
+
+double mvm::Shared::data::getNumber()
+{
+  return m_number;
+}
+
+string mvm::Shared::data::getString()
+{
+  return m_string;
+}
+
 } // namespace arken
