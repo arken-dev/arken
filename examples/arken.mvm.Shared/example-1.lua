d3ad4a23a673f0e1bc3f7c4474c36f4b941749dc
Alexandre Fresneda Riveira <alexandre.riveira@gmail.com>
2022-11-26 09:16:21 -0300

    example

--- examples/arken.mvm.Shared/example-1.lua
+++ examples/arken.mvm.Shared/example-1.lua
@@ -1,5 +1,5 @@
 local mvm = require('arken.mvm')
-local instance = mvm.current()
+local instance = mvm.current():shared()
 print(instance)
-instance:shared():setString('hello', 'world!')
+instance::setString('hello', 'world!')
 print(mvm.current():shared():getString('hello'))
