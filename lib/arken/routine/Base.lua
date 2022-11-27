64f409e643a26a03df8e08a69bab89b4e7461cfd
Alexandre F Riveira <alexandre@objectdata.com.br>
2022-11-26 21:30:09 -0300

    Base

--- lib/arken/routine/Base.lua
+++ lib/arken/routine/Base.lua
@@ -1,6 +1,13 @@
 local Class = require("arken.oop.Class")
 local Base  = Class.new("arken.routine.Base")
 
+function Base.new(params)
+  local obj = { __params = params }
+  setmetatable(obj, class)
+  obj:initialize()
+  return obj
+end
+
 function Base:params(rebuild)
   if self.__params == nil or rebuild then
     local routine = require('arken.routine')
