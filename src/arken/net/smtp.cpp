9ac4f24d2b5b452eb87789db451d5bbe00fb073a
Alexandre F Riveira <alexandre@objectdata.com.br>
2023-05-17 13:11:22 -0300

    fix arken.net.SMTP

--- src/arken/net/smtp.cpp
+++ src/arken/net/smtp.cpp
@@ -123,9 +123,9 @@ bool SMTP::perform()
       url.append("smtp://");
     }
 
-    curl_easy_setopt(curl, CURLOPT_PORT, m_port);
+    //curl_easy_setopt(curl, CURLOPT_PORT, m_port);
     url.append(m_url).append(":").append(std::to_string(m_port));
-    curl_easy_setopt(curl, CURLOPT_URL, m_url.data());
+    curl_easy_setopt(curl, CURLOPT_URL, url.data());
 
     // ???
     curl_easy_setopt(curl, CURLOPT_HTTPAUTH, CURLAUTH_NONE);
