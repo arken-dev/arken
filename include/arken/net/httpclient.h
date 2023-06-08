50207b1411d633475866c5dc77b47dc3c4978d7d
Alexandre F Riveira <alexandre@objectdata.com.br>
2023-06-08 19:03:25 -0300

    httpclient

--- include/arken/net/httpclient.h
+++ include/arken/net/httpclient.h
@@ -24,6 +24,7 @@ class HttpClient
   string       m_cert;
   string       m_certKey;
   bool         m_verbose;
+  bool         m_formdata;
   bool         m_sslVerifyPeer;
   long         m_sslVerifyHost;
   long         m_sslVersion;
