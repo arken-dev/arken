fa961134b7f1ece0102acee293bdbcb7d581643f
Alexandre F Riveira <alexandre@objectdata.com.br>
2024-02-29 12:00:42 -0300

    openssl

---
+++ cmake/openssl.cmake
@@ -0,0 +1,11 @@
+# ------------------------------------------------------------------------------
+#
+# Copyright 2016 The Arken Platform Authors.
+#
+# All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+#
+# ------------------------------------------------------------------------------
+set(LIBCRYPTO_PATH "" CACHE STRING "alternativa path for libcrypto")
+set(LIBSSL_PATH "" CACHE STRING "alternativa path for libssl")
