3b1870242641b54f99048cd1fb6c344ab7942319
Alexandre F Riveira <alexandre@objectdata.com.br>
2024-05-08 09:30:14 -0300

    manager

--- profile.d/manager.lua
+++
@@ -1,7 +0,0 @@
-MANAGER_PATH  = '/opt/objectdata/manager'
-empty = require('arken.empty')
-package.path = package.path .. ";" .. MANAGER_PATH .. "/app/?.lua"
-package.path = package.path .. ";" .. MANAGER_PATH .. "/lib/?.lua"
-package.path = package.path .. ";" .. MANAGER_PATH .. "/app/channels/?.lua"
-
-Class = require('arken.oop.Class')
