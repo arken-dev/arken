5a82358163ced62196ead4efa4e6cafc00524426
Alexandre F Riveira <alexandre@objectdata.com.br>
2025-10-06 10:27:47 -0300

    diff

--- src/arken/string.cpp
+++ src/arken/string.cpp
@@ -2112,6 +2112,12 @@ const char * string::List::operator[](int pos)
 
 const char * string::List::at(int pos)
 {
+
+   if( pos > 10000 ) {
+    std::cout << "achei" << std::endl;
+    return nullptr;
+  }
+
   if( pos > m_size ) {
     return nullptr;
   }
