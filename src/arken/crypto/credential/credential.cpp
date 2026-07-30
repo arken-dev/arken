58025e6ff1ec8abb47d33c7bce3b0880711ce3ec
Alexandre Riveira <alexandre@objectdata.com.br>
2026-07-30 20:54:37 -0300

    credential

--- src/arken/crypto/credential/credential.cpp
+++ src/arken/crypto/credential/credential.cpp
@@ -19,19 +19,27 @@ void crypto::Credential::load(string fileName)
   using string = arken::string;
   using List   = arken::string::List;
 
-
-
-  if ( os::exists(fileName) ) {
+  if ( ! os::exists(fileName) ) {
     throw "file credential not exists";
   } else{
+	  std::cout << "estou aqui" << std::endl;
     string value = os::read(fileName);
-    string data  = aes::decrypt(value, ARKEN_CRYPTO_AES_KEY);
+    //std::cout << "size " << value.size() << "dpeois do read" << value << std::endl;
+    string data;
+    try {
+      data  = aes::decrypt(value, "abc123");//ARKEN_CRYPTO_AES_KEY);
+    } catch (const char *msg) {
+      std::cout << "deu ruim " << msg << std::endl;
+    }
+    std::cout << "decript----------------------" << std::endl;
+    std::cout << data << std::endl;
     List   list  = data.split("\n");
 
     for( int i=0; i< list.size(); i++ ) {
       string row = list[i];
       string key = row.prefix("=").squish();
       string val = row.suffix("=").squish();
+      std::cout << "key " << key << "value " << value << std::endl;
       s_credential->insert({key, val});
     }
   }
