d027550349126cccbe7f4b06f14fb177cdd62b54
RicardoCarriel <rick01.carriel@gmail.com>
2026-07-30 15:18:02 -0300

    arken.crypto.aes implementation

---
+++ cmake/arken-crypto.cmake
@@ -0,0 +1,13 @@
+# ------------------------------------------------------------------------------
+#
+# Copyright 2016 The Arken Platform Authors.
+#
+# All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+#
+# ------------------------------------------------------------------------------
+
+set(ARKEN_CRYPTO_AES_KEY "" CACHE STRING "arken.crypto.aes default key, used when encrypt/decrypt is called without a password")
+
+message("ARKEN_CRYPTO_AES_KEY........: ${ARKEN_CRYPTO_AES_KEY}")
